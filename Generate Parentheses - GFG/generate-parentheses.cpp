d1fecd749002e1f0ed8960be1e29bee86106e6fe
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-01-15 21:27:54 +0530

    Added solution - LeetHub

---
+++ Generate Parentheses - GFG/generate-parentheses.cpp
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<string> AllParenthesis(int n) ;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+// N is the number of pairs of parentheses
+// Return list of all combinations of balanced parantheses
+class Solution
+{
+    public:
+    
+    void solve(string s,int i,int j,vector<string> &ans)
+    {
+        if(i==0 && j==0)
+        {
+            ans.push_back(s);
+            return;
+        }
+        
+        if(i>j)
+        {
+            return;
+        }
+        
+        if(i==0)
+        {
+            solve(s+')',i,j-1,ans);
+            return;
+        }
+        
+        solve(s+'(',i-1,j,ans);
+        solve(s+')',i,j-1,ans);
+    }
+    
+    vector<string> AllParenthesis(int n) 
+    {
+        vector<string> ans;
+        solve("",n,n,ans);
+        
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		Solution ob;
+		vector<string> result = ob.AllParenthesis(n); 
+		sort(result.begin(),result.end());
+		for (int i = 0; i < result.size(); ++i)
+			cout<<result[i]<<"\n";
+	}
+	return 0; 
+} 
+  // } Driver Code Ends
