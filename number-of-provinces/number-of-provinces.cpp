6827198bed7f9ce5c058d9e220204b7149b23cb9
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-01-30 10:36:30 +0530

    Time: 20 ms, Memory: 13.7 MB - LeetHub

---
+++ number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    
+    vector<int> v;
+    
+  
+    
+    int find(int x)
+    {
+        while(x!=v[x])
+        {
+            x=v[x];
+        }
+        
+        return x;
+    }
+    
+    void Union(int x,int y)
+    {
+        int a=find(x);
+        int b=find(y);
+        
+        if(a!=b)
+        {
+            v[b]=a;
+        }
+    }
+    int findCircleNum(vector<vector<int>>& a) {
+        
+        int n=a.size();
+        int m=a[0].size();
+        
+        for(int i=0;i<n;i++)
+        {
+            v.push_back(i);
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(a[i][j]==1)
+                {
+                    Union(i,j);
+                }
+            }
+        }
+        
+        int c=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(v[i]==i)
+            {
+                c++;
+            }
+        }
+        
+        return c;
+    }
+};
