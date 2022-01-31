aa86e4374e5906f6f97b6756d3c23a6dab03af3f
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-01-31 08:52:59 +0530

    Time: 11 ms (19.66%), Space: 7.9 MB (13.11%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& a) {
+        
+        int n=a.size();
+        int m=a[0].size();
+        
+        int ans=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            int b=0;
+            
+            for(int j=0;j<m;j++)
+            {
+                b+=a[i][j];
+            }
+            
+            ans=max(ans,b);
+        }
+        
+        return ans;
+    }
+};
