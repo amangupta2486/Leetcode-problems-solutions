45713a9c21dff869051ce71f83cbb7934d771a5e
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-02-03 23:07:31 +0530

    Time: 44 ms (84.11%), Space: 13.2 MB (50.88%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,68 +1,65 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& a, int b) {
+    vector<vector<int>> fourSum(vector<int>& a, int t) {
         
-    int n=a.size();
-    vector<vector<int>> ans;
-    
-        if(n<4)
-        {
-            return ans;
-        }
+        int n=a.size();
+        vector<vector<int>> ans;
         
-    sort(a.begin(),a.end());
-    for(int i=0;i<n;i++)
-    {
-        for(int j=i+1;j<n;j++)
+        sort(a.begin(),a.end());
+        
+        for(int i=0;i<n;i++)
         {
-            int l=j+1;
-            int r=n-1;
-            int k=b-a[i]-a[j];
-
-            while(l<r)
+            for(int j=i+1;j<n;j++)
             {
-                int s=a[l]+a[r];
-
-                if(s<k)
+                int l=j+1,h=n-1;
+                
+                int b=t-a[i]-a[j];
+                
+                while(l<h)
                 {
-                    l++;
-                }
-                else if(s>k)
-                {
-                    r--;
-                }
-
-                else
-                {
-                    int x=a[l];
-                    int y=a[r];
-
-                    ans.push_back({a[i],a[j],a[l],a[r]});
-
-                    while(l<r && a[l]==x)
+                    int s = a[l]+a[h];
+                    
+                    if(s<b)
                     {
                         l++;
                     }
-                    while(l<r && a[r]==y)
+                    
+                    else if(s>b)
                     {
-                        r--;
+                        h--;
                     }
-
+                    
+                    else
+                    {
+                        int x=a[l];
+                        int y=a[h];
+                        
+                        ans.push_back({a[i],a[j],a[l],a[h]});
+                            
+                        while(l<h && a[l]==x)
+                        {
+                            l++;
+                        }
+                        
+                        while(l<h && a[h]==y)
+                        {
+                            h--;
+                        }
+                    }
+                }
+                
+                while(j+1<n && a[j]==a[j+1])
+                {
+                    j++;
                 }
             }
-
-            while(j+1<n && a[j]==a[j+1])
+            
+            while(i+1<n && a[i]==a[i+1])
             {
-                j++;
+                i++;
             }
         }
-
-        while(i+1<n && a[i]==a[i+1])
-        {
-            i++;
-        }
-    }
-
-    return ans;
+        
+        return ans;
     }
 };
