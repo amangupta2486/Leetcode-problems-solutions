510301b6cd873069400f1996a203df07182a35a5
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-02-07 08:57:43 +0530

    Time: 11 ms (6.98%), Space: 6.9 MB (9.15%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -3,17 +3,36 @@ public:
     char findTheDifference(string s, string t) {
         
         int n=s.length();
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
         
-        for(int i=0;i<n;i++)
+//         for(int i=0;i<n;i++)
+//         {
+//             if(s[i]!=t[i])
+//             {
+//                 return t[i];
+//             }
+//         }
+        
+        map<char,int> m1,m2;
+        
+        for(auto i:s)
         {
-            if(s[i]!=t[i])
+            m1[i]++;
+        }
+        
+        for(auto i:t)
+        {
+            m2[i]++;
+        }
+        
+        for(int i=0;i<n+1;i++)
+        {
+            if(m2[t[i]]>m1[t[i]])
             {
                 return t[i];
             }
         }
-        
         return t[n];
     }
 };
