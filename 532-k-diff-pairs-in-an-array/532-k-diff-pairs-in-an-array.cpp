2dd54097fa9ab6787c41da82bc0f5fce5bfec88f
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-02-09 09:57:24 +0530

    Time: 1663 ms (5.16%), Space: 17.8 MB (5.12%) - LeetHub

---
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findPairs(vector<int>& a, int k) {
+        
+        int n=a.size();
+        
+        int c=0;
+        
+        map<vector<int>,int> m;
+        
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(abs(a[i]-a[j])==k)
+                {
+                    vector<int> v;
+                    v.push_back(a[i]);
+                    v.push_back(a[j]);
+                    if(v[0]>v[1])
+                    {
+                        swap(v[0],v[1]);
+                    }
+                    m[v]++;
+                    c++;
+                }
+            }
+        }
+        
+        return m.size();
+    }
+};
