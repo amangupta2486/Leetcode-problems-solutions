e83bd92bc919b0ed3ee758c01a9c4bb04f619a43
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-02-15 19:14:39 +0530

    Time: 85 ms (28.16%), Space: 18.9 MB (56.88%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& p, int h) {
+    int minEatingSpeed(vector<int>& p, int k) {
         
         int n=p.size();
-
-        int l=1,r=*max_element(p.begin(),p.end());
         
-        while(l<r)
+        int l=1,h=*max_element(p.begin(),p.end());
+        
+        //int ans=h;
+        
+        while(l<h)
         {
-            int m=(l+r)/2;
-
+            int m=(l+h)/2;
+            
             int x=0;
             
-            for(auto i:p)
+            for(int i=0;i<n;i++)
             {
-                x+= (i/m)+(i%m!=0);
+                x+=(p[i]/m);
+               
+                if(p[i]%m)
+                {
+                    x++;
+                }
             }
-            if(x<=h)
+            
+            if(x<=k)
             {
-                r=m;
+                //ans=min(ans,m);
+                h=m;
             }
             
             else
             {
                 l=m+1;
             }
-       
         }
         
-        return r;
-
+        return h;
     }
 };
