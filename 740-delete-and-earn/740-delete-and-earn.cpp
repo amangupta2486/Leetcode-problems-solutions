2be4c6819fee6d15f7aadb2db52c5b7f5094960b
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-03-05 09:46:38 +0530

    Time: 8 ms (77.52%), Space: 9.3 MB (77.38%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -4,27 +4,21 @@ public:
         
         int n=a.size();
         
-        if(n==1)
-        {
-            return a[0];
-        }
-        
         int mx=*max_element(a.begin(),a.end());
         
-        vector<int> f(mx+1,0),dp(mx+1,0);
+        vector<int> f(mx+2,0),dp(mx+2,0);
         
         for(auto i:a)
         {
             f[i]++;
         }
         
-        dp[1] = f[1];
-        
+        dp[1]=f[1];
         dp[2]=max(dp[1],f[2]*2);
         
         for(int i=3;i<=mx;i++)
         {
-            dp[i]=max(dp[i-1],dp[i-2]+(i*f[i]));
+            dp[i]=max(dp[i-1],dp[i-2]+(f[i]*i));
         }
         
         return dp[mx];
