193aae4f1c6dab549502fde73cac76f4070b74fa
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-03-30 17:29:30 +0530

    Time: 4 ms (77.73%), Space: 9.6 MB (52.00%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& g, int t) {
+        
+        int n=g.size();
+        int m=g[0].size();
+        
+        int i=0,j=m-1;
+        
+        while(i<n && j>=0)
+        {
+            if(g[i][j]==t)
+            {
+                return 1;
+            }
+            
+            if(g[i][j]<t)
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        
+        return 0;
+    }
+};
