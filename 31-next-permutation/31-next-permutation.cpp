086501ad41de3d142e99338246ede012bc1cf5c6
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-04-03 10:56:35 +0530

    Time: 11 ms (19.49%), Space: 12.1 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     void nextPermutation(vector<int>& a) {
         
-        int i=0;
-        int f=0;
         int n=a.size();
         
-        for(i=n-2;i>=0;i--)
+        int f=0;
+        
+        int i=n-2;
+        
+        for(;i>=0;i--)
         {
             if(a[i]<a[i+1])
             {
                 f=1;
+               // i++;
                 break;
             }
         }
         
         if(f)
         {
-         
             for(int j=n-1;j>i;j--)
             {
                 if(a[j]>a[i])
@@ -27,12 +29,14 @@ public:
                 }
             }
         }
+        cout<<i<<" ";
         
-
-        reverse(a.begin()+i+1,a.end());
-           
-        //sort(a.begin()+i+1,a.end());
+        if(f)
+        {
+            sort(a.begin()+i+1,a.end());
+        }
         
-
+        else
+        sort(a.begin(),a.end());
     }
 };
