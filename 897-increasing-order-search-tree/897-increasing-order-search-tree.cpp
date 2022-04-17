dc962a68f0ca5b9918226a30325145432974fa32
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-04-17 10:37:18 +0530

    Time: 6 ms (20.69%), Space: 7.7 MB (63.55%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -12,10 +12,8 @@
 class Solution {
 public:
     
-    vector<int> v;
-    
-//     TreeNode* h;
-//     TreeNode* r=h;
+    TreeNode* h=new TreeNode(1);
+    TreeNode* r=h;
     
     void solve(TreeNode* root)
     {
@@ -25,28 +23,19 @@ public:
         }
         
         solve(root->left);
-        v.push_back(root->val);
-        //h=h->right=new TreeNode(root->val);
+        h=h->right=new TreeNode(root->val);
         solve(root->right);
     }
     
     TreeNode* increasingBST(TreeNode* root) {
         
-        //h=root;
         if(root==NULL)
         {
             return root;
         }
         
         solve(root);
-        
-        TreeNode* h=new TreeNode(1);
-        TreeNode* r=h;
-        
-        for(auto i:v)
-        {
-            h=h->right=new TreeNode(i);
-        }
+
         return r->right;
         
     }
