bf615465f45d2f27c295b4f3d89f5ea1f343be8b
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-07 15:20:39 +0530

    Time: 37 ms (41.63%), Space: 14.5 MB (5.35%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& a, int t) {
+        
         int n=a.size();
         
-        map<int,int> m;
+        map<int,int> mp;
         
         for(auto i:a)
         {
-            m[i]++;
+            mp[i]++;
         }
         
-        vector<int> v;
+        vector<int>v;
         
         for(int i=0;i<n;i++)
         {
-            if(2*a[i]==t && m[a[i]]<2)
+             if(a[i]*2==t && mp[a[i]]<2)
             {
                 continue;
             }
             
-            if(m[t-a[i]]>0)
+            else if(mp[t-a[i]]>0)
             {
-                v.push_back(i);
+              v.push_back(i);
             }
         }
         
