1c5a85067e0680ca18464fa9de09638402a4e37f
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-08 12:33:30 +0530

    Time: 7 ms (6.53%), Space: 6.6 MB (33.84%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     vector<vector<int>> generate(int n) {
         
-        vector<vector<int>> ans(n);
-        
+        vector<vector<int>> ans;
+
         for(int i=0;i<n;i++)
         {
-            
+            vector<int> v;
             for(int j=0;j<=i;j++)
             {
                 if(j==0 || j==i)
                 {
-                    ans[i].push_back(1);
+                    v.push_back(1);
                 }
-                
                 else
                 {
-                    ans[i].push_back(ans[i-1][j-1]+ans[i-1][j]);
+                    v.push_back(ans[i-1][j-1]+ans[i-1][j]);
                 }
             }
+            ans.push_back(v);
         }
         
         return ans;
