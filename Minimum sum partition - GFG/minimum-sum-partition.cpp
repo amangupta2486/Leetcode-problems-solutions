2a3c7969baaa8f9984a2aeafda9e308840ed4525
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-09 19:43:31 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -9,51 +9,47 @@ class Solution{
 	int minDifference(int a[], int n)  { 
 	    
 	    int s=0;
-	    
 	    for(int i=0;i<n;i++)
-	    {
-	        s+=a[i];
-	    }
-	    
-	    bool dp[n+1][s+1];
-	    
-	    for(int i=0;i<=s;i++)
-	    {
-	        dp[0][i]=0;
-	    }
-	    
-	    for(int i=0;i<=n;i++)
-	    {
-	        dp[i][0]=1;
-	    }
-	    
-	    for(int i=1;i<=n;i++)
-	    {
-	        for(int j=1;j<=s;j++)
-	        {
-	            if(a[i-1]<=j)
-	            {
-	                dp[i][j] = dp[i-1][j-a[i-1]] || dp[i-1][j];
-	            }
-	            else
-	            {
-	                dp[i][j]=dp[i-1][j];
-	            }
-	        }
-	    }
-	    
-	    vector<int> v;
-	    
-	    for(int j=0;j<=s/2;j++)
-	    {
-	        if(dp[n][j])
-	        {
-	            v.push_back(j);
-	        }
-	    }
-	    
-	    int d = v[v.size()-1];
-	    return s-2*d;
+        {
+            s+=a[i];
+        }
+        int sum=s;
+        
+        bool dp[n+1][s+1];
+        
+        memset(dp,0,sizeof(dp));
+
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=1;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=s;j++)
+            {
+                if(a[i-1]<=j)
+                {
+                    dp[i][j]=dp[i-1][j] || dp[i-1][j-a[i-1]];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        
+        int mx=0;
+        
+        for(int j=1;j<=s/2;j++)
+        {
+            if(dp[n][j])
+            {
+                mx=max(mx,j);
+            }
+        }
+        
+        return s-(2*mx);
 	} 
 };
 
