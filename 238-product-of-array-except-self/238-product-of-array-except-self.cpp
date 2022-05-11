372f5350a9f03c2db5ca9617450634512a1ff7d3
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-11 15:38:18 +0530

    Time: 25 ms (80.36%), Space: 24.1 MB (56.99%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -4,22 +4,18 @@ public:
         
         int n=a.size();
         
-        vector<int> p(n,1),s(n,1);
+        vector<int> p(n,1);
         
         for(int i=1;i<n;i++)
         {
             p[i]=p[i-1]*a[i-1];
         }
         
+        int sp=1;
         for(int i=n-2;i>=0;i--)
         {
-            s[i]=s[i+1]*a[i+1];
-        }
-        
-        for(int i=0;i<n;i++)
-        {
-            //cout<<p[i]<<" "<<s[i]<<endl;
-            p[i]=p[i]*s[i];
+            sp*=a[i+1];
+            p[i]*=sp;
         }
         
         return p;
