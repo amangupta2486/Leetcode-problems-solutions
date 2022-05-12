f38771d82e79cb880b8e8c8517f964e1b8ebe924
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-12 12:41:40 +0530

    Time: 87 ms (45.28%), Space: 16.7 MB (75.59%) - LeetHub

---
+++ 221-maximal-square/221-maximal-square.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& g) {
+        
+        int n=g.size();
+        int m=g[0].size();
+        
+        int dp[n+2][m+2];
+        
+        memset(dp,0,sizeof(dp));
+        
+        int ans=0;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(g[i-1][j-1]=='1')
+                {
+                    dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+                }
+                //cout<<dp[i][j]<<" ";
+                ans=max(ans,dp[i][j]);
+            }
+            //cout<<endl;
+        }
+        
+        return ans*ans;
+    }
+};
