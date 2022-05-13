015a4bbfbe17be3d5add8ff3654fcc967e5a3392
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-13 19:06:03 +0530

    Time: 117 ms (12.62%), Space: 7.5 MB (69.91%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    int dp[505][505];
+    
+    int solve(string &a, string &b,int n,int m)
+    {
+        if(n==0)
+        {
+            return m;
+        }
+        
+        if(m==0)
+        {
+            return n;
+        }
+        
+        if(dp[n][m]!=-1)
+        {
+            return dp[n][m];
+        }
+        if(a[n-1]==b[m-1])
+        {
+            return dp[n][m]=solve(a,b,n-1,m-1);
+        }
+        
+        return dp[n][m]=1+min({solve(a,b,n-1,m-1),solve(a,b,n-1,m),solve(a,b,n,m-1)});
+    }
+    
+    int minDistance(string a, string b) {
+        
+        int n=a.size();
+        int m=b.size();
+        memset(dp,-1,sizeof(dp));
+        
+        return solve(a,b,n,m);
+    }
+};
