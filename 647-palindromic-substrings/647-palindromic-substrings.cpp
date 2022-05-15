ab494188d94286315efa8cc0e9af7284b16b224e
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-16 00:23:45 +0530

    Time: 19 ms (47.12%), Space: 7.3 MB (60.90%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,29 +1,54 @@
 class Solution {
 public:
-    
-    void solve(int i,int j,int &ans,string &s)
-    {
-        int n=s.size();
+
+     int countSubstrings(string s){
+         int n=s.size();
+         
+         bool dp[n+1][n+1];
+         memset(dp,0,sizeof(dp));
+         
+         for(int i=0;i<n;i++)
+         {
+             dp[i][i]=1;
+         }
+         
+         
+         for(int i=0;i<n-1;i++)
+         {
+             if(s[i]==s[i+1])
+             {
+                 dp[i][i+1]=1;
+             }
+         }
         
-        while(i>=0 && j<n && s[i]==s[j])
-        {
-            ans++;
-            i--;
-            j++;
-        }
-    }
-    int countSubstrings(string s) {
-        
-        int n=s.size();
-        
-        int ans=0;
-        
-        for(int i=0;i<n;i++)
-        {
-            solve(i,i,ans,s);
-            solve(i,i+1,ans,s);
-        }
-        
-        return ans;
-    }
+         for(int k=3;k<=n;k++)
+         {
+             for(int i=0;i<n-k+1;i++)
+             {
+                 int j= i+k-1;
+                 
+                 if(s[i]==s[j] && dp[i+1][j-1])
+                 {
+                     dp[i][j]=1;
+                 }
+             }
+         }
+             
+         int ans=0;
+         
+         for(int i=0;i<n;i++)
+         {
+             for(int j=0;j<n;j++)
+             {
+                 if(dp[i][j])
+                 {
+                     ans++;
+                 }
+             }
+         }
+         
+         return ans;
+             
+             
+     }
 };
