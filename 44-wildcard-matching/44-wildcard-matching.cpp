9d78ead821b902b03c4a1e81e157be290c428d98
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-16 12:31:33 +0530

    Time: 32 ms (85.53%), Space: 11.3 MB (54.90%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -18,38 +18,22 @@ public:
             }
         }
         
-       // int j=1;
-        
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
-            {    
-            if(s[i-1]==p[j-1] || p[j-1]=='?')
             {
-                dp[i][j]=dp[i-1][j-1];
+                if(s[i-1]==p[j-1] || p[j-1]=='?')
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                
+                if(p[j-1]=='*')
+                {
+                    dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                }
             }
-
-            if(p[j-1]=='*')
-            {
-                dp[i][j]=dp[i-1][j]||dp[i][j-1];
-            }
-         }
         }
         
         return dp[n][m];
     }
 };
-
-
-
-// Input
-// "adceb"
-// "*a*b"
-// Output
-// false
-// Expected
-// true
-
-
-// "ab"
-// "?*"
