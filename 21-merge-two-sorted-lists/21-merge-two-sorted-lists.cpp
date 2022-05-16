bb7d7e8752169498d8b7749bbcf1b09ca49db494
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-16 19:47:20 +0530

    Time: 13 ms (34.34%), Space: 14.8 MB (44.23%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -24,24 +24,30 @@ public:
         
         if(l1->val>l2->val)
         {
-            std::swap(l1,l2);    
+            ListNode* t=l1;
+            l1=l2;
+            l2=t;
+            //std::swap(l1,l2);
         }
         
         ListNode* res=l1;
         
         while(l1!=NULL && l2!=NULL)
         {
-            ListNode* temp=NULL;
+            ListNode* tmp=NULL;
             
-            while(l1!=NULL && l1->val<= l2->val)
+            while(l1!=NULL && l1->val<=l2->val)
             {
-                temp=l1;
+                tmp=l1;
                 l1=l1->next;
             }
             
-            temp->next=l2;
+            tmp->next=l2;
             
-            std::swap(l1,l2);
+            ListNode* t=l1;
+            l1=l2;
+            l2=t;
+            //std::swap(l1,l2);
         }
         
         return res;
