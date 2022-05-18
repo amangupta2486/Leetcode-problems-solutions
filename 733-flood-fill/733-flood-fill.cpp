53922f6bff4bb2040eb7d7a046fd5d3136e81b8c
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-18 15:29:42 +0530

    Time: 24 ms (7.09%), Space: 14.2 MB (37.99%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& g, int sr, int sc, int l) {
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         
-        int n=g.size();
-        int m=g[0].size();
+        int givencol=image[sr][sc];
         
-        int c=0;
-        
-        c=g[sr][sc];
-        
-        if(c==l)
+        if(givencol==newColor)
         {
-            return g;
+            return image;    
         }
         
-        int dx[4]={0,0,1,-1};
-        int dy[4]={1,-1,0,0};
+        int n=image.size();
+        int m=image[0].size();
         
         queue<vector<int>> q;
+        image[sr][sc]=newColor;
         
         q.push({sr,sc});
-        g[sr][sc]=l;
+        
+        int dx[4]={1,-1,0,0};
+        int dy[4]={0,0 ,1,-1};
         
         while(!q.empty())
         {
             int k=q.size();
+            
             while(k--)
             {
                 auto p=q.front();
@@ -32,27 +31,21 @@ public:
                 
                 int i=p[0];
                 int j=p[1];
-                              
-                for(int z=0;z<4;z++)
+                
+                for(int k=0;k<4;k++)
                 {
-                    int x=i+dx[z];
-                    int y=j+dy[z];
+                    int x=i+dx[k];
+                    int y=j+dy[k];
                     
-                    if(x>=0 && x<n && y>=0 && y<m && g[x][y]==c)
+                    if(x>=0 && x<n && y>=0 && y<m && image[x][y]==givencol)
                     {
-                        g[x][y]=l;
+                        image[x][y]=newColor;
                         q.push({x,y});
                     }
                 }
             }
         }
         
-        return g;
+        return image;
     }
 };
-
-
-// [[0,0,0],[0,1,1]]
-// 1
-// 1
-// 1
