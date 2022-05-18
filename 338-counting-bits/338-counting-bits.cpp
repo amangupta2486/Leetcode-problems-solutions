64c20d7051afcdfec05cbdf554d75ce953e15f54
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-18 23:25:37 +0530

    Time: 8 ms (52.05%), Space: 8 MB (48.18%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -9,10 +9,7 @@ public:
         if(n>0)
         dp[1]=1;
         
-        if(n>1)
-        dp[2]=1;
-        
-        for(int i=3;i<=n;i++)
+        for(int i=2;i<=n;i++)
         {
             dp[i]=dp[i/2]+dp[i%2];
         }
