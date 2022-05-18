dbce90d9e11883e524a3ea8e260059b61f4d4c57
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-18 19:21:46 +0530

    Time: 37 ms (29.72%), Space: 13.7 MB (66.82%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -2,47 +2,50 @@ class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& p) {
         
-        vector<int> d(n,0);
-        
-        vector<int> v[n];
-        
-        for(auto i:p)
-        {
-            v[i[1]].push_back(i[0]);
-            d[i[0]]++;
-        }
-        
-        queue<int> q;
-        int ans=0;
-        
-        for(int i=0;i<n;i++)
-        {
-            if(d[i]==0)
-            {
-                q.push(i);
-                ans++;
-            }
-        }
-        
-        while(!q.empty())
-        {
-            auto r=q.front();
-            q.pop();
-            
-            for(auto x:v[r])
-            {
-                d[x]--;
-                
-                if(d[x]==0)
-                {
-                    q.push(x);
-                    ans++;
-                }
-            }
-        }
-        
-        cout<<ans<<endl;
-        
-        return ans==n;
+         vector<int> d(n,0);
+	    vector<int> v[n];
+	    
+	    for(auto i:p)
+	    {
+	        v[i[1]].push_back(i[0]);
+	        d[i[0]]++;
+	    }
+	    
+	    queue<int> q;
+	    
+	    int ans=0;
+	    
+	    for(int i=0;i<n;i++)
+	    {
+	        if(d[i]==0)
+	        {
+	            q.push(i);
+	            ans++;
+	        }
+	    }
+	    
+	    while(!q.empty())
+	    {
+	        int k=q.size();
+	        
+	        while(k--)
+	        {
+	            auto p=q.front();
+	            q.pop();
+	            
+	            for(auto x:v[p])
+	            {
+	                d[x]--;
+	                
+	                if(d[x]==0)
+	                {
+	                    q.push(x);
+	                    ans++;
+	                }
+	            }
+	        }
+	    }
+	    
+	    return ans==n ? 1 : 0;
     }
 };
