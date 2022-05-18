558becf187648ab92f36fc1e3a7aae9da3dc9793
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-18 19:49:28 +0530

    Time: 281 ms (38.27%), Space: 54.7 MB (61.49%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.cpp
+++ 310-minimum-height-trees/310-minimum-height-trees.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& e) {
         
+        vector<int> d(n,0);
+        vector<int> v[n];
+        
         if(n==1)
         {
-            vector<int> v;
-            v.push_back(0);
-            
-            return v;
+            return {0};
         }
-        vector<int> d(n,0);
-        
-        vector<int> v[n];
         
         for(auto i:e)
         {
@@ -22,7 +19,6 @@ public:
         }
         
         queue<int> q;
-        
         vector<int> ans;
         
         for(int i=0;i<n;i++)
@@ -30,31 +26,28 @@ public:
             if(d[i]==1)
             {
                 q.push(i);
-                ans.push_back(i);
-                d[i]--;
             }
         }
         
         while(!q.empty())
         {
             int k=q.size();
-            
             ans.clear();
+            
             while(k--)
             {
                 auto p=q.front();
                 q.pop();
-                ans.push_back(p);
-                //cout<<p;
                 
-                for(auto x : v[p])
+                ans.push_back(p);
+                
+                for(auto x:v[p])
                 {
                     d[x]--;
                     
                     if(d[x]==1)
                     {
                         q.push(x);
-                        
                     }
                 }
             }
