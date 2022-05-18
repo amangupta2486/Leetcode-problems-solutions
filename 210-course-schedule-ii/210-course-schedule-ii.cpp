b7066f7aa273f0f161a36929a2d2cbbf2748e2d3
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-18 19:25:53 +0530

    Time: 56 ms (9.56%), Space: 13.9 MB (61.69%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -3,51 +3,56 @@ public:
     vector<int> findOrder(int n, vector<vector<int>>& p) {
         
         vector<int> d(n,0);
+	    vector<int> v[n];
+	    
+	    for(auto i:p)
+	    {
+	        v[i[1]].push_back(i[0]);
+	        d[i[0]]++;
+	    }
+	    
+	    queue<int> q;
+	    
+	    int ans=0;
+	    
+        vector<int> order;
         
-        vector<int> v[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        if(d[i]==0)
+	        {
+	            q.push(i);
+                order.push_back(i);
+	            ans++;
+	        }
+	    }
+	    
+	    while(!q.empty())
+	    {
+	        int k=q.size();
+	        
+	        while(k--)
+	        {
+	            auto p=q.front();
+	            q.pop();
+	            
+	            for(auto x:v[p])
+	            {
+	                d[x]--;
+	                
+	                if(d[x]==0)
+	                {
+	                    q.push(x);
+                        order.push_back(x);
+	                    ans++;
+	                }
+	            }
+	        }
+	    }
+	    
+        vector<int> b;
         
-        for(auto i:p)
-        {
-            v[i[1]].push_back(i[0]);
-            d[i[0]]++;
-        }
+	    return ans==n ? order : b;
         
-        queue<int> q;
-        int ans=0;
-        vector<int> g,h;
-        
-        for(int i=0;i<n;i++)
-        {
-            if(d[i]==0)
-            {
-                q.push(i);
-                g.push_back(i);
-                ans++;
-            }
-        }
-        
-        while(!q.empty())
-        {
-            auto r=q.front();
-            q.pop();
-            
-            for(auto x:v[r])
-            {
-                d[x]--;
-                
-                if(d[x]==0)
-                {
-                    q.push(x);
-                    g.push_back(x);
-                    ans++;
-                }
-            }
-        }
-        
-       // cout<<ans<<endl;
-        if(ans!=n)
-            return h;
-        
-        return g;
     }
 };
