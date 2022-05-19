f89b88eed053e57edb3daa3ad8750d577a9c4871
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-19 15:44:26 +0530

    Time: 31 ms (5.01%), Space: 21.3 MB (74.87%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -14,74 +14,30 @@ public:
     
     int ans=0;
     
-    void solve(TreeNode* root,int s, int t)
+    void solve(TreeNode* root, int t,int s)
     {
         if(root==NULL)
         {
-            return;
+            return; 
         }
         
         s+=root->val;
         
         if(root->left==NULL && root->right==NULL)
         {
-            if(s==t)
-            {
-                ans=1;
-            }    
+            if(t==s)
+            ans=1;
             return;
         }
         
-        
-        solve(root->left,s,t);
-        solve(root->right,s,t);
+        solve(root->left,t,s);
+        solve(root->right,t,s);
     }
     bool hasPathSum(TreeNode* root, int t) {
         
-        if(root==NULL)
-        {
-            return 0;
-        }
+
+        solve(root,t,0);
         
-       // solve(root,0,t);
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,root->val});
-        
-        while(!q.empty())
-        {
-            int k=q.size();
-            
-            while(k--)
-            {
-                auto r=q.front();
-                q.pop();
-                
-                auto p=r.first;
-                int  s=r.second;
-                
-                //s+=root->val;
-               // cout<<s<<" ";
-                
-                if(p->left==NULL && p->right==NULL)
-                {
-                    if(s==t)
-                    {
-                        return 1;
-                    }
-                }
-                
-                if(p->left!=NULL)
-                {
-                    q.push({p->left,s+p->left->val});
-                }
-                
-                if(p->right!=NULL)
-                {
-                    q.push({p->right,s+p->right->val});
-                }
-            }
-        }
-        cout<<endl;
         return ans;
     }
 };
