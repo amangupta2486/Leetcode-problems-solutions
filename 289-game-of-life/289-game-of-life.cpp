af4141c197040d2046afe98c5f9585fb3579011d
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-21 00:34:27 +0530

    Time: 6 ms (15.78%), Space: 6.9 MB (85.67%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,73 +1,62 @@
 class Solution {
 public:
-    void gameOfLife(vector<vector<int>>& g) {
+    
+    int dx[8]={1,1,-1,-1,0,0,1,-1};
+    int dy[8]={1,-1,-1,1,1,-1,0,0};
+    
+    void gameOfLife(vector<vector<int>>& b) {
         
-        int n=g.size();
-        int m=g[0].size();
+        int n=b.size();
+        int m=b[0].size();
         
-        int dx[8]={1,1,-1,-1,1,-1,0,0};
-        int dy[8]={-1,1,-1,1,0,0,1,-1};
+      
         
-        vector<vector<int>> b(n,vector<int>(m));
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                b[i][j]=g[i][j];
-                //cout<<b[i][j]<<" ";
-            }
-            //cout<<endl;
-        }
-        
+        vector<vector<int>> v(n,vector<int>(m,0));
         
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<m;j++)            
             {
+  
                 int c=0;
-                
+               
                 for(int k=0;k<8;k++)
                 {
                     int x=i+dx[k];
                     int y=j+dy[k];
                     
-                    if(x>=0 && x<n && y>=0 && y<m && g[x][y]==1)
+                    if(x>=0 && x<n && y>=0 && y<m && b[x][y]==1)
                     {
                         c++;
                     }
                 }
-                //cout<<c<<" ";
-                if(g[i][j]==1)
+                 //cout<<i<<" "<<j<<" "<<c<<endl;
+                if(b[i][j]==1)
                 {
                     if(c<2)
                     {
-                        b[i][j]=0;
+                        v[i][j]=0;
                     }
-                    
-                    if(c>3)
+                    else if(c==2 || c==3)
                     {
-                        b[i][j]=0;
+                        v[i][j]=1;
+                    }
+                    else if(c>3)
+                    {
+                        v[i][j]=0;
                     }
                 }
-                else
+                else if(b[i][j]==0)
                 {
                     if(c==3)
                     {
-                        b[i][j]=1;
+                        v[i][j]=1;
                     }
                 }
-            }
-            //cout<<endl;
+            }   
         }
         
-       for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                g[i][j]=b[i][j];
-            }
-        }
-         
+        b=v;
+        
     }
 };
