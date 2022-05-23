55b1f48a2e272fde1365fb46ac517b01434d2909
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-23 13:11:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (70.43%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        
-        int f=0,c=0;
-
-        if(n<0)
-        {
-            return 0;
-        }
-        for(int i=0;i<32;i++)
-        {
-            int x=1<<i;
-            //cout<<x<<" ";
-            if((x&n))
-            {
-                c++;
-            }
-            
-        }
-        
-       // cout<<c<<endl;
-        
-        return c==1 ? 1 : 0;
+      
+      if(n<=0)
+      {
+          return 0;
+      }
+    
+      int x=log2(n);
+      //cout<<x<<endl;
+      return pow(2,x)==n ? 1 :0;
     }
 };
