1982d52fae3b1f6f2446c3a806932c18d2aacc14
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-23 23:48:32 +0530

    Time: 48 ms (88.72%), Space: 14.9 MB (87.83%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,53 +1,47 @@
 class Solution {
 public:
     
-    int vis[205][205];
     int dp[205][205];
-    
     int dx[4]={1,-1,0,0};
     int dy[4]={0,0,1,-1};
     
-     int dfs(vector<vector<int>>& g,int i,int j,int n,int m)
-     {
-         
+    int dfs(vector<vector<int>>& g,int i,int j,int n,int m)
+    {
         if(dp[i][j]!=-1)
         {
             return dp[i][j];
         }
-         
-         int ans=0;
-         
+        int ans=0;
+        
         for(int k=0;k<4;k++)
-         {
-             int x=i+dx[k];
-             int y=j+dy[k];
-
-             if(x>=0 && x<n && y>=0 && y<m && g[x][y]>g[i][j])
-             {
-                ans= max(ans,1+dfs(g,x,y,n,m));
-             }
-         }
+        {
+            int x=i+dx[k];
+            int y=j+dy[k];
+            
+            if(x>=0 && x<n && y>=0 && y<m && g[x][y]>g[i][j])
+            {
+                ans=max(ans,1+dfs(g,x,y,n,m));
+            }
+        }
         
         return dp[i][j]=ans;
-     }
-   
+    }
     int longestIncreasingPath(vector<vector<int>>& g) {
         
         int n=g.size();
         int m=g[0].size();
-        memset(vis,0,sizeof(vis));
-        memset(dp,-1,sizeof(dp));
         
         int ans=0;
+        memset(dp,-1,sizeof(dp));
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                ans=max(ans,dfs(g,i,j,n,m));
+                ans=max(ans,1+dfs(g,i,j,n,m));
             }
         }
         
-        return ans+1;
+        return ans;
     }
 };
