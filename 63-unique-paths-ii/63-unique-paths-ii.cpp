f078136f9c276f501f7962a1676a6f5a0067808f
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-24 11:55:40 +0530

    Time: 3 ms (82.43%), Space: 7.7 MB (75.33%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -3,30 +3,35 @@ public:
     
     int dp[105][105];
     
-    int solve(vector<vector<int>>& g,int i,int j,int n,int m)
+    int solve(vector<vector<int>> &g,int i,int j,int n,int m)
     {
+       
         if(i>=n || j>=m || g[i][j]==1)
         {
             return 0;
+        }
+        
+         if(i==n-1 && j==m-1)
+        {
+            return 1;
         }
         
         if(dp[i][j]!=-1)
         {
             return dp[i][j];
         }
-        if(i==n-1 && j==m-1)
-        {
-            return 1;
-        }
         
-        return dp[i][j]=solve(g,i+1,j,n,m) + solve(g,i,j+1,n,m);
+        return dp[i][j]=solve(g,i+1,j,n,m)+ solve(g,i,j+1,n,m);
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& g) {
+    
+    int uniquePathsWithObstacles(vector<vector<int>> &g) {
         
         int n=g.size();
         int m=g[0].size();
         
         memset(dp,-1,sizeof(dp));
+        
         return solve(g,0,0,n,m);
     }
 };
+
