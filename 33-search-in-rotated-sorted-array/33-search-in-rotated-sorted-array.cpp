0be10b8e09d9d2b295ff2c74b04d9eca969a56d0
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-25 10:09:36 +0530

    Time: 8 ms (31.12%), Space: 11.1 MB (30.08%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,49 @@
 class Solution {
 public:
     
-    int findKRotation(vector<int>& a, int n) {
-	    
-	    int i=0,j=n-1;
-	    
-	    while(i<=j)
-	    {
-	        int m=(i+j)/2;
-	        
-	        if(a[m]<=a[(m+1)%n] && a[m]<=a[(m+n-1)%n])
-	        {
-	            return m;
-	        }
-	        
-	        if(a[m]>=a[0])
-	        {
-	            i=m+1;
-	        }
-	        else
-	        {
-	            j=m-1;
-	        }
-	    }
-	    
-	    return 0;
-	}
+     int findMin(vector<int>& a) {
+        
+        int n=a.size();
+        int i=0,j=n-1;
+        
+        if(n==1)
+        {
+            return 0;
+        }
+         
+        if(n==2)
+        {
+           if(a[0]<=a[1])
+           {
+               return 0;
+           }
+            
+           else
+               return 1;
+        }
+        
+        while(i<=j)
+        {
+            int m=(i+j)/2;
+            
+            if(a[m]<=a[(n+(m-1)%n)%n] && a[m]<=a[(m+1)%n])
+            {
+                return m;
+            }
+            
+            else if(a[m]>a[0])
+            {
+                i++;
+            }
+            
+            else if(a[m]<a[n-1])
+            {
+                j--;
+            }
+        }
+        
+        return 0;
+    }
     
     int solve(vector<int>& a,int i,int j,int t)
     {
@@ -52,25 +70,27 @@ public:
     }
     int search(vector<int>& a, int t) {
         
+        
+        int m=findMin(a);
+        
+        if(a[m]==t)
+        {
+            return m;
+        }
         int n=a.size();
         
-        int mi=findKRotation(a,n);
+        int l=solve(a,0,m,t);
+        int r=solve(a,m+1,n-1,t);
         
-        if(a[mi]==t)
+        if(l!=-1)
         {
-            return mi;
+            return l;
+        }
+        if(r!=-1)
+        {
+            return r;
         }
         
-        int x=solve(a,mi,n-1,t);
-        int y=solve(a,0,mi-1,t);
-        
-        if(x==-1 && y==-1)
-        {
-            return -1;
-        }
-        
-        return abs(x*y);
-
-       
+        return -1;
     }
 };
