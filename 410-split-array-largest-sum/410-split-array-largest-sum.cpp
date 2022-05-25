9d4b96e19a36f483350ed7d1fee1c19850008786
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-25 18:08:40 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (41.02%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -4,39 +4,39 @@ public:
         
         int n=a.size();
         
-        int l=*max_element(a.begin(),a.end());
-        int h=0;
+        int l=0,r=0;
         
         for(int i=0;i<n;i++)
         {
-            h+=a[i];
+            l=max(l,a[i]);
+            r+=a[i];
         }
         
-        int mi=0;
+        int ans=r;
         
-        while(l<=h)
+        while(l<=r)
         {
-            int m = (l+h)/2;
+            int m=(l+r)/2;
             
-            int s=0,mx=0,c=1;
+            int s=0,c=1;
             
             for(int i=0;i<n;i++)
             {
-                s+=a[i];
-                
-                if(s>m)
+                if(s+a[i]<=m)
+                {
+                    s+=a[i];
+                }
+                else
                 {
                     s=a[i];
                     c++;
                 }
-                
-                mx=max(mx,s);
             }
             
             if(c<=k)
             {
-                mi=m;
-                h=m-1;
+                ans=m;
+                r=m-1;
             }
             else
             {
@@ -44,6 +44,6 @@ public:
             }
         }
         
-        return mi;
+        return ans;
     }
 };
