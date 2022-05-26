78c67c1b04584a6e9b1f35334953c643c2f46643
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-27 00:01:24 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (76.43%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,19 +4,16 @@ public:
         
         int n=a.size();
         
-        if(n<3)
-        {
-            return *max_element(a.begin(),a.end());
-        }
-        
-        vector<int> dp(n,0);
+        vector<int> dp(n+1,0);
         
         dp[0]=a[0];
+        
+        if(n>1)
         dp[1]=max(a[0],a[1]);
         
         for(int i=2;i<n;i++)
         {
-            dp[i]=max(a[i]+dp[i-2],dp[i-1]);
+            dp[i]=max(dp[i-1],dp[i-2]+a[i]);
         }
         
         return dp[n-1];
