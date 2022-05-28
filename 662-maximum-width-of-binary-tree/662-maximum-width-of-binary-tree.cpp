d3c408192ca258aa7d39c5caba484ea4dda5badb
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-28 18:50:06 +0530

    Time: 13 ms (47.11%), Space: 17.4 MB (22.04%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -13,49 +13,42 @@ class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
         
-        queue<pair<TreeNode*,long long>> q;
+        if(root==NULL)
+        {
+            return 0;
+        }
         
-        q.push({root,0});
+        queue<pair<TreeNode*,long>> q;
+        q.push({root,1});
         
-        long long ans=0;
+        long ans=0;
         
         while(!q.empty())
         {
+            long first=INT_MAX,last=0;
             int k=q.size();
             
-            auto mi= q.front().second;
-            
-            long long l=0,r=0;
-            
-            for(int j=0;j<k;j++)
+            while(k--)
             {
                 auto p=q.front();
                 q.pop();
                 
-                auto s=p.first;
-                long long i=p.second-mi;
+                auto r=p.first;
+                long i=p.second;
+                first=min(first,p.second);
+                last=max(last,p.second);
                 
-                if(j==0)
+                if(r->left!=NULL)
                 {
-                    l=i;
+                    q.push({r->left,((2*i))-first});
                 }
-                
-                if(j==k-1)
+                if(r->right!=NULL)
                 {
-                    r=i;
-                }
-                
-                if(s->left!=NULL)
-                {
-                    q.push({s->left,(2*i)+1});
-                }
-                if(s->right!=NULL)
-                {
-                    q.push({s->right,(2*i)+2});
+                     q.push({r->right,((2*i)+1)-first});
                 }
             }
             
-            ans=max(ans,r-l+1);
+            ans=max(ans,last-first+1);
         }
         
         return ans;
