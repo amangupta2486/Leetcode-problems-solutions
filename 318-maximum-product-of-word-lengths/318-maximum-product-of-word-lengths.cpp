0d89e1a5b88387995ee14cad2a93b21d916975b8
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-29 09:45:15 +0530

    Time: 1275 ms (5.40%), Space: 167.6 MB (5.14%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maxProduct(vector<string>& w) {
+        
+        int n=w.size();
+        int ans=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            map<char,int> mp1;
+            int m=w[i].size();
+            for(auto l:w[i])
+            {
+                mp1[l]++;
+            }
+            for(int j=i+1;j<n;j++)
+            {
+                string x=w[j];
+                int f=1;
+                int m2=x.size();
+                //cout<<w[i]<<" "<<x<<endl;
+                for(auto k:x)
+                {
+                    if(mp1.find(k)!=mp1.end())
+                    {
+                        f=0;
+                        break;
+                    }
+                }
+                
+                if(f)
+                {
+                    //cout<<w[i]<<" "<<x<<endl;
+                    ans=max(ans,m*m2);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
