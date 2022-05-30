0896028de1e65f9c9a122774263ea2a22b5900df
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-05-30 23:06:28 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (66.61%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -2,38 +2,33 @@ class Solution {
 public:
     bool backspaceCompare(string s, string t) {
         
+        string a="",b="";
         int n=s.size();
         int m=t.size();
         
-        string a="",b="";
-        
         for(int i=0;i<n;i++)
         {
-            if(s[i]=='#' && a.size()!=0)
+            if(s[i]=='#' && a.size()>0)
             {
                 a.pop_back();
             }
-            else
+            else if(s[i]!='#')
             {
-                if(s[i]!='#')
                 a+=s[i];
             }
         }
         
         for(int i=0;i<m;i++)
         {
-            if(t[i]=='#' && b.size()!=0)
+            if(t[i]=='#' && b.size()>0)
             {
                 b.pop_back();
             }
-            else
+            else if(t[i]!='#')
             {
-                if(t[i]!='#')
                 b+=t[i];
             }
         }
-        
-        //cout<<a<<" "<<b<<endl;
         
         return a==b;
     }
