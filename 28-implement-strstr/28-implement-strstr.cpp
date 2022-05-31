a1884a583f63d11caa80115e5e944ae17f948cbe
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-01 00:20:09 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (59.99%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -4,38 +4,33 @@ public:
         
         int n=a.size();
         int m=b.size();
-        int f=0,start=0;
         
-        if(m>n)
+        if(n<m)
         {
             return -1;
         }
         
-        for(int i=0;i<n;i++)
+        int f=1,start=0;
+        
+        for(int i=0;i<n-m+1;i++)
         {
-            if(a[i]==b[0] && n-i>=m)
+            int j=i+m-1;
+            
+            f=1;
+            int h=0;
+            
+            for(int k=i;k<=j;k++)
             {
-                f=1;
-                cout<<i<<endl;
-                start=i;
-                int k=i;
-                for(int j=0;j<m && k<n;k++,j++)
+                if(a[k]!=b[h])
                 {
-                   // cout<<a[k]<<" "<<b[j]<<endl;
-                    if(a[k]!=b[j])
-                    {
-                       // cout<<k<<" "<<j<<endl;
-                      //  cout<<a[k]<<" "<<b[j]<<endl;
-                       
-                        f=0;
-                        break;
-                    }
-                }            
-               //  cout<<f<<endl;
+                    f=0;
+                    break;
+                }
+                h++;
             }
-            if(f)
+            if(f==1)
             {
-               // cout<<f<<endl;
+                start=i;
                 break;
             }
         }
