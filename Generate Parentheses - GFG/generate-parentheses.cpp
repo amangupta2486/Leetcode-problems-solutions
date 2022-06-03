21f939ea88d96d708fce6451b11c8f83262a8cae
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-03 19:12:01 +0530

    Added solution - LeetHub

--- Generate Parentheses - GFG/generate-parentheses.cpp
+++ Generate Parentheses - GFG/generate-parentheses.cpp
@@ -15,33 +15,31 @@ class Solution
 {
     public:
     
-    void solve(string s,int i,int j,vector<string> &ans)
+    vector<string> ans;
+    
+    void solve(string s,int i,int j)
     {
         if(i==0 && j==0)
         {
             ans.push_back(s);
             return;
         }
-        
+        if(i==0)
+        {
+            solve(s+')',i,j-1);
+            return;
+        }
         if(i>j)
         {
             return;
         }
-        
-        if(i==0)
-        {
-            solve(s+')',i,j-1,ans);
-            return;
-        }
-        
-        solve(s+'(',i-1,j,ans);
-        solve(s+')',i,j-1,ans);
+        solve(s+'(',i-1,j);
+        solve(s+')',i,j-1);
     }
     
     vector<string> AllParenthesis(int n) 
     {
-        vector<string> ans;
-        solve("",n,n,ans);
+        solve("",n,n);
         
         return ans;
     }
