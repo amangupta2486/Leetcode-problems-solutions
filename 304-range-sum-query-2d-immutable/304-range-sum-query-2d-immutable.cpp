2308cf1c993eec39effb0311cdd1a8c30f30ac20
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-03 09:24:16 +0530

    Time: 476 ms (75.35%), Space: 251.5 MB (5.15%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,38 +1,45 @@
 class NumMatrix {
 public:
     
-    
-    //int dp[205][205];
-    vector<vector<int>> dp;
-    
+    int dp[1005][1005];
     NumMatrix(vector<vector<int>>& g) {
-        //memset(dp,0,sizeof(dp));
         
         int n=g.size();
         int m=g[0].size();
         
-        dp.resize(n+1,vector<int>(m+1,0));
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                dp[i][j]=dp[i][j-1]+g[i-1][j-1];
-            }
-        }
+        //int dp[n+1][m+1];
+        memset(dp,0,sizeof(dp));
         
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
-                dp[i][j]+=dp[i-1][j];
+                dp[i][j]+=dp[i][j-1]+g[i-1][j-1];
             }
         }
+        
 
+            for(int j=1;j<=m;j++)
+            {
+               for(int i=1;i<=n;i++)
+               {
+                   dp[i][j]+=dp[i-1][j];
+               }
+            }
+    
+        // for(int i=1;i<=n;i++)
+        // {
+        //     for(int j=1;j<=m;j++)
+        //     {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
     }
     
     int sumRegion(int r1, int c1, int r2, int c2) {
         
-        long ans=dp[r2+1][c2+1]-dp[r1][c2+1]-dp[r2+1][c1]+dp[r1][c1];
+        long long ans=dp[r2+1][c2+1]-dp[r1][c2+1]-dp[r2+1][c1]+dp[r1][c1];
         return ans;
     }
 };
