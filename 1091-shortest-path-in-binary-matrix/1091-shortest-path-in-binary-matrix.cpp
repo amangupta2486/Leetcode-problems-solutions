4f2c2c47a3f3a1bb560c8279bd2dd060ef9930b3
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-07 00:03:45 +0530

    Time: 199 ms (22.20%), Space: 36.8 MB (20.76%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,48 +1,28 @@
 class Solution {
 public:
     
-    int ans=INT_MAX;
-    int dx[8]={1,1,-1,-1,0,0 ,-1,1};
-    int dy[8]={0,1, 0,-1,1,-1,1,-1};
-    // int dx[8]={1,1,1,-1,-1,-1,0,0};
-    // int dy[8]={0,1,-1,1,-1,0,1,-1};
-    /*
-    void solve(vector<vector<int>>& g,int i,int j,int n,int m,int c)
-    {
-        if(i==n-1 && j==m-1)
-        {
-            ans=min(ans,c);
-        }
-        
-        for(int k=0;k<8;k++)
-        {
-            int x=i+dx[k];
-            int y=j+dy[k];
-            
-            if(x>=0 && x<n && y>=0 && y<m && !g[x][y])
-            {
-                g[x][y]=1;
-                solve(g,x,y,n,m,c+1);
-                g[x][y]=0;
-            }
-        }
-    }
-    */
+    int dx[8]={1,1,1,-1,-1,-1,0,0};
+    int dy[8]={-1,0,1,-1,0,1,-1,1};
+    
     int shortestPathBinaryMatrix(vector<vector<int>>& g) {
+        
         int n=g.size();
-       // int m=g[0].size();
+        int m=g[0].size();
         
-//         if(g[0][0]==0)
-//         solve(g,0,0,n,m,1);
+        int ans=0;
         
-//         else
         if(g[0][0]==1)
+        {
             return -1;
+        }
+        
+        if(n==1 && m==1 && g[0][0]==0)
+        {
+            return 1;
+        }
         
         queue<vector<int>> q;
         q.push({0,0,1});
-        
-        int f=0;
         
         while(!q.empty())
         {
@@ -52,36 +32,30 @@ public:
             {
                 auto p=q.front();
                 q.pop();
+                
                 int i=p[0];
                 int j=p[1];
                 int c=p[2];
                 
-                if(i==n-1 && j==n-1)
+                if(i==n-1 && j==m-1)
                 {
-                    ans=c;
-                    f=1;
-                    break;
+                    return c;
                 }
+                
                 for(int k=0;k<8;k++)
                 {
                     int x=i+dx[k];
                     int y=j+dy[k];
-
-                    if(x>=0 && x<n && y>=0 && y<n && g[x][y]==0)
+                    
+                    if(x>=0 && x<n && y>=0 && y<m && g[x][y]==0)
                     {
                         g[x][y]=1;
                         q.push({x,y,c+1});
                     }
                 }
             }
-            
-            if(f)
-            break;
         }
         
-        if(ans==INT_MAX)
-            return -1;
-        
-        return ans;
+        return -1;
     }
 };
