ed53c6ff4e545c713dfd6317f1baf1c527dc692d
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-09 17:04:19 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -5,83 +5,48 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-    // Function to detect cycle in a directed graph
-    
+    // Function to detect cycle in a directed graph.
     int vis[100005];
+    int d[100005];
     
-    bool ans=false;
-    bool dfs(int i,int p,vector<int> adj[])
+    bool dfs(int i,vector<int> adj[])
     {
         vis[i]=1;
-       // cout<<i<<"->";
-        for(auto j:adj[i])
+        d[i]=1;
+        for(auto x:adj[i])
         {
-            if(!vis[j])
+            if(!vis[x])
             {
-                dfs(j,i,adj);
+                if(dfs(x,adj))
+                return true;
             }
-            else
+            else if(d[x])
             {
-                ans=true;
+                return true;
             }
         }
+        d[i]=0;
+       // d[x]=0;
+        
+        return false;
     }
-    
     bool isCyclic(int V, vector<int> adj[]) {
-        
-        memset(vis,0,sizeof(vis));
-        
-       // dfs(0,-1,adj);
+       int n=V;
+       memset(vis,0,sizeof(vis));
+       memset(d,0,sizeof(d));
        
-	   vector<int> d(V,0);
-	    
-	    for(int i=0;i<V;i++)
-	    {
-	        for(auto x:adj[i])
-	        {
-	            d[x]++;
-	        }
-	    }
-	    
-	    
-	    queue<int> q;
-	    
-	    for(int i=0;i<V;i++)
-	    {
-	        if(d[i]==0)
-	        {
-	            q.push(i);
-	        }
-	    }
-	    
-	    vector<int> ans;
-	    int c=0;
-	    
-	    while(!q.empty())
-	    {
-	        int k=q.size();
-	        
-	        while(k--)
-	        {
-	            auto p=q.front();
-	            q.pop();
-	            
-	            c++;
-	            ans.push_back(p);
-	            
-	            for(auto x:adj[p])
-	            {
-	                d[x]--;
-	                
-	                if(d[x]==0)
-	                {
-	                    q.push(x);
-	                }
-	            }
-	        }
-	    }
-
-	    return c==V ? 0 : 1;
+       for(int i=0;i<n;i++)
+       {
+           if(!vis[i])
+           {
+               if(dfs(i,adj))
+               {
+                   return true;
+               }
+           }
+       }
+       
+       return false;
     }
 };
 
