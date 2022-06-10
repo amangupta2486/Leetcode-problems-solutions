b095c2dc75c5bb73a3aeaf699ed7a1f7db51686d
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-10 16:54:23 +0530

    Time: 238 ms (57.40%), Space: 78 MB (63.31%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -31,8 +31,30 @@ public:
         
         int n=p.size();
         // int dp[n+1][2];
-        memset(dp,-1,sizeof(dp));
+        memset(dp,0,sizeof(dp));
         
-        return solve(0,p,1,2);
+        //return solve(0,p,1,2);
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int buy=0;buy<2;buy++)
+            {
+                for(int k=1;k<=2;k++)
+                {
+                    int profit=0;
+                    if(buy)
+                    {
+                        profit=max(-p[i]+dp[i+1][0][k],dp[i+1][1][k]);
+                    }
+                    else
+                    {
+                        profit=max(p[i]+dp[i+1][1][k-1],dp[i+1][0][k]);
+                    }
+                    dp[i][buy][k]=profit;
+                }
+            }
+        }
+        
+        return dp[0][1][2];
     }
 };
