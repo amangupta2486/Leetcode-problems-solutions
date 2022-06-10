19698b0661aafb0a0bac5ccbee596165be245eb1
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-10 20:09:51 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -6,34 +6,33 @@ using namespace std;
 class Solution{
 
 	public:
-	int perfectSum(int a[], int n, int s)
+	int perfectSum(int a[], int n, int sum)
 	{
-	    int mod=1e9 +7;
-        int dp[n+1][s+1];
-        
+        int dp[n+1][sum+1];
         memset(dp,0,sizeof(dp));
         
+        int mod=1e9+7;
+        
         for(int i=0;i<n;i++)
-        {
-            dp[i][0]=1;
-        }
+        dp[i][0]=1;
         
         for(int i=1;i<=n;i++)
         {
-            for(int j=0;j<=s;j++)
+            for(int j=0;j<=sum;j++)
             {
                 if(a[i-1]<=j)
                 {
-                    dp[i][j]=(dp[i-1][j] + dp[i-1][j-a[i-1]])%mod;
+                    dp[i][j]= (dp[i-1][j-a[i-1]] +  dp[i-1][j])%mod;
                 }
                 else
                 {
-                    dp[i][j]=dp[i-1][j]%mod;
+                     dp[i][j]= dp[i-1][j]%mod;
                 }
+              //  dp[i][j]=dp[i][j]%mod;
             }
         }
         
-        return dp[n][s]%mod;
+        return dp[n][sum]%mod;
 	}
 	  
 };
