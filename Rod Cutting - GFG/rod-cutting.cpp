f2b47d4ab76181a6c625f66c67ec04311d858e6a
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-10 19:51:56 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -10,30 +10,30 @@ using namespace std;
 
 class Solution{
   public:
-    int cutRod(int p[], int n) {
-        //code here
-        int dp[n+1][n+1];
-        int ans=0;
+    int cutRod(int val[], int n) {
+        
+        int w=n;
+        
+        int dp[n+1][w+1];
         memset(dp,0,sizeof(dp));
         
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=n;j++)
+            for(int j=1;j<=w;j++)
             {
                 if(i<=j)
                 {
-                    dp[i][j]=max(p[i-1]+dp[i][j-i],dp[i-1][j]);
+                    dp[i][j]= max(val[i-1]+dp[i][j-i],dp[i-1][j]);
                 }
                 else
                 {
-                    dp[i][j]=dp[i-1][j];
+                     dp[i][j]= dp[i-1][j];
                 }
                 
-                ans=max(ans,dp[i][j]);
             }
         }
         
-        return dp[n][n];
+        return dp[n][w];
     }
 };
 
