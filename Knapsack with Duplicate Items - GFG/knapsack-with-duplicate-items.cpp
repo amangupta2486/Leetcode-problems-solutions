d9bb6ead033fc2fe7a1cf2036fa76ea2d3f9db49
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-10 19:49:05 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,40 +9,8 @@ using namespace std;
 
 class Solution{
 public:
-    
-    int ans=0;
-    int dp[1005][1005];
-    
-    int solve(int i,int s,int n, int w, int val[], int wt[],int c)
-    {
-        if(i<0 || s>=w)
-        {
-            return 0;
-        }
-        
-        if(dp[i][s]!=-1)
-        {
-            return dp[i][s];
-        }
-        
-        int l=0,r=0;
-        
-        if(s+wt[i]<=w)
-        {
-            l= max(val[i]+solve(i,s+wt[i],n,w,val,wt,c+val[i]),solve(i-1,s,n,w,val,wt,c));
-        }
-        
-        r=solve(i-1,s,n,w,val,wt,c);
-        
-        return dp[i][s]=max(l,r);
-    }
     int knapSack(int n, int w, int val[], int wt[])
     {
-        
-        memset(dp,-1,sizeof(dp));
-        return solve(n-1,0,n,w,val,wt,0);
-
-        /*
         int dp[n+1][w+1];
         memset(dp,0,sizeof(dp));
         
@@ -52,17 +20,17 @@ public:
             {
                 if(wt[i-1]<=j)
                 {
-                    dp[i][j]=max(val[i-1]+dp[i][j-wt[i-1]],dp[i-1][j]);
+                    dp[i][j]= max(val[i-1]+dp[i][j-wt[i-1]],dp[i-1][j]);
                 }
                 else
                 {
-                    dp[i][j]=dp[i-1][j];
+                     dp[i][j]= dp[i-1][j];
                 }
+                
             }
         }
         
         return dp[n][w];
-        */
     }
 };
 
