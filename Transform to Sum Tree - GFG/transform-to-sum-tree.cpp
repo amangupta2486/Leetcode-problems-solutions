01a2c53e9ea48283eccf10a6a7d90a4663d55ef6
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-10 09:41:04 +0530

    Added solution - LeetHub

--- Transform to Sum Tree - GFG/transform-to-sum-tree.cpp
+++ Transform to Sum Tree - GFG/transform-to-sum-tree.cpp
@@ -108,28 +108,25 @@ class Solution {
     // Convert a given tree to a tree where every node contains sum of values of
     // nodes in left and right subtrees in the original tree
     
-    int solve(Node* root)
+    int  solve(Node* root)
     {
         if(root==NULL)
         {
             return 0;
         }
-        
         int l=0,r=0;
-        if(root->left!=NULL)
-        l=+root->left->data+solve(root->left);
         
-        if(root->right!=NULL)
-        r=root->right->data+solve(root->right);
+        l=solve(root->left);
+        r=solve(root->right);
         
-        root->data= l+r;
+        int ans=root->data+l+r;
+        root->data=l+r;
         
-        return l+r;
+        return ans;
     }
     void toSumTree(Node *root)
     {
-       solve(root);
-       
+        solve(root);
     }
 };
 
