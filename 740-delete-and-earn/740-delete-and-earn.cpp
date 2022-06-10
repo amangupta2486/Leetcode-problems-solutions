f309dd12ad8d51f3b84c2e3120bee3fd6ce90a5d
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-10 14:27:24 +0530

    Time: 21 ms (53.29%), Space: 13.7 MB (52.87%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,32 +1,10 @@
 class Solution {
 public:
-    
-    map<int,int> mp;
-    int dp[10005];
-    
-    int solve(int i,vector<int> &a)
-    {
-        if(i==0)
-            return 0;
-        
-        if(i==1)
-            return mp[1];
-        
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
-        
-        int l=mp[i]*i + solve(i-2,a);
-        int r=solve(i-1,a);
-        
-        return dp[i]=max(l,r);
-    }
     int deleteAndEarn(vector<int>& a) {
         
         int n=a.size();
+        unordered_map<int,int> mp;
         int mx=0;
-        memset(dp,-1,sizeof(dp));
         
         for(int i=0;i<n;i++)
         {
@@ -34,6 +12,16 @@ public:
             mx=max(mx,a[i]);
         }
         
-        return solve(mx,a);
+        vector<int> dp(mx+1,0);
+        
+        dp[0]=0;
+        dp[1]=mp[1];
+        
+        for(int i=2;i<=mx;i++)
+        {
+            dp[i]=max(dp[i-2]+(mp[i]*i),dp[i-1]);
+        }
+        
+        return dp[mx];
     }
 };
