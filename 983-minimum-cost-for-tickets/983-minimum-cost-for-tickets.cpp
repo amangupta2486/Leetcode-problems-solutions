d5494d4c6cd03e590c79d95d776da669684e31db
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-10 14:38:52 +0530

    Time: 40 ms (5.03%), Space: 20.6 MB (5.64%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
+    map<int,int> mp;
+    int dp[400];
     
-    vector<int> dp;
-    
-    int solve(vector<int>& d, vector<int>& c,int j)
+    int solve(int i,vector<int>& c)
     {
-        int n=d.size();
-        
-        if(j>=n)
+        if(i>365)
         {
             return 0;
         }
         
-        if(dp[j])
+        if(dp[i]!=0)
         {
-            return dp[j];
+            return dp[i];
         }
-        int x=c[0]+solve(d,c,j+1);
-        
-        int i=0;
-        
-        for(i=j;i<n && d[i]<d[j]+7;i++);
-        
-        int y=c[1]+solve(d,c,i);
-        
-        for(i=j;i<n && d[i]<d[j]+30;i++);
-        
-        int z=c[2]+solve(d,c,i);
 
-        int ans=min({x,y,z});
+        int ans=0;
         
-        return dp[j]=ans;
+        if(mp[i]>0)
+        {
+            ans =min({c[0]+solve(i+1,c),c[1]+solve(i+7,c),c[2]+solve(i+30,c)});
+        }
+        else
+        {
+            ans = solve(i+1,c);
+        }
+        
+        return dp[i]=ans;
     }
     int mincostTickets(vector<int>& d, vector<int>& c) {
         
         int n=d.size();
         
-        dp.resize(n+1);
+        for(int i=0;i<n;i++)
+        {
+            mp[d[i]]++;
+        }
+        memset(dp,0,sizeof(dp));
         
-        return solve(d,c,0);
+        return solve(1,c);
     }
 };
