5de1e473bf3636b4d9e22cb01384465b6d4579ea
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-11 10:54:57 +0530

    Time: 29 ms (58.14%), Space: 13.7 MB (69.92%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,51 +1,51 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& p) {
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         
-         vector<int> d(n,0);
-	    vector<int> v[n];
-	    
-	    for(auto i:p)
-	    {
-	        v[i[1]].push_back(i[0]);
-	        d[i[0]]++;
-	    }
-	    
-	    queue<int> q;
-	    
-	    int ans=0;
-	    
-	    for(int i=0;i<n;i++)
-	    {
-	        if(d[i]==0)
-	        {
-	            q.push(i);
-	            ans++;
-	        }
-	    }
-	    
-	    while(!q.empty())
-	    {
-	        int k=q.size();
-	        
-	        while(k--)
-	        {
-	            auto p=q.front();
-	            q.pop();
-	            
-	            for(auto x:v[p])
-	            {
-	                d[x]--;
-	                
-	                if(d[x]==0)
-	                {
-	                    q.push(x);
-	                    ans++;
-	                }
-	            }
-	        }
-	    }
-	    
-	    return ans==n ? 1 : 0;
+        int n=numCourses;
+        
+        vector<int> d(n,0);
+        vector<int> v[n];
+        
+        for(auto i:prerequisites)
+        {
+            v[i[1]].push_back(i[0]);
+            d[i[0]]++;
+        }
+        
+        queue<int> q;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(d[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        
+        int cnt=0;
+        
+        while(!q.empty())
+        {
+            int k=q.size();
+            while(k--)
+            {
+                auto p=q.front();
+                q.pop();
+                
+                cnt++;
+                
+                for(auto x:v[p])
+                {
+                    d[x]--;
+                    if(d[x]==0)
+                    {
+                        q.push(x);
+                    }
+                }
+            }
+        }
+        
+        return cnt==n ? 1 :0 ;
     }
 };
