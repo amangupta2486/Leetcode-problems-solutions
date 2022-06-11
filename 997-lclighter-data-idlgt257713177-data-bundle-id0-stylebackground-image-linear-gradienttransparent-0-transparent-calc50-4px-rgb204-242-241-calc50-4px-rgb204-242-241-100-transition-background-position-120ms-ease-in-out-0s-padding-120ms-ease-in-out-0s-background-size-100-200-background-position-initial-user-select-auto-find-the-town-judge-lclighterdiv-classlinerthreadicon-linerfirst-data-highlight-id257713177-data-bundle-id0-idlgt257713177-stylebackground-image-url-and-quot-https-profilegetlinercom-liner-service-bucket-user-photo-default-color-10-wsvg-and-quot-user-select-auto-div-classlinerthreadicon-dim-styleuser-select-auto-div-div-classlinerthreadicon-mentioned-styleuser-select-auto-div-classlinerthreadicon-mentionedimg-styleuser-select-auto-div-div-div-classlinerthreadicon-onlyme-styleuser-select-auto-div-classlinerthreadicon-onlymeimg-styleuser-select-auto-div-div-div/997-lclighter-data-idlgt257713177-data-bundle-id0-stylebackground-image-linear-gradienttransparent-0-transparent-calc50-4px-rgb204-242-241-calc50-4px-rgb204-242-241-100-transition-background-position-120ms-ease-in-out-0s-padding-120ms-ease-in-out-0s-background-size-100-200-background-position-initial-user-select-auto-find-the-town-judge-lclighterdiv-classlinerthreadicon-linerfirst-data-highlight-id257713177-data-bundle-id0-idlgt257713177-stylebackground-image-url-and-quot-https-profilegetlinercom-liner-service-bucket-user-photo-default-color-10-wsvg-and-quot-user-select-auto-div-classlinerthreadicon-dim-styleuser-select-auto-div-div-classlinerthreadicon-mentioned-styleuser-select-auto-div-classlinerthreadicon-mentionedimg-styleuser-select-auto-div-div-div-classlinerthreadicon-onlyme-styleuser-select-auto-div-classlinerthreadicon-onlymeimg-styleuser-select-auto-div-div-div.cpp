ada0ed1e70fd02cfb0e72c9304ee86dcb6db56ac
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-11 11:46:11 +0530

    Time: 395 ms (6.84%), Space: 69 MB (21.61%) - LeetHub

---
+++ 997-lclighter-data-idlgt257713177-data-bundle-id0-stylebackground-image-linear-gradienttransparent-0-transparent-calc50-4px-rgb204-242-241-calc50-4px-rgb204-242-241-100-transition-background-position-120ms-ease-in-out-0s-padding-120ms-ease-in-out-0s-background-size-100-200-background-position-initial-user-select-auto-find-the-town-judge-lclighterdiv-classlinerthreadicon-linerfirst-data-highlight-id257713177-data-bundle-id0-idlgt257713177-stylebackground-image-url-and-quot-https-profilegetlinercom-liner-service-bucket-user-photo-default-color-10-wsvg-and-quot-user-select-auto-div-classlinerthreadicon-dim-styleuser-select-auto-div-div-classlinerthreadicon-mentioned-styleuser-select-auto-div-classlinerthreadicon-mentionedimg-styleuser-select-auto-div-div-div-classlinerthreadicon-onlyme-styleuser-select-auto-div-classlinerthreadicon-onlymeimg-styleuser-select-auto-div-div-div/997-lclighter-data-idlgt257713177-data-bundle-id0-stylebackground-image-linear-gradienttransparent-0-transparent-calc50-4px-rgb204-242-241-calc50-4px-rgb204-242-241-100-transition-background-position-120ms-ease-in-out-0s-padding-120ms-ease-in-out-0s-background-size-100-200-background-position-initial-user-select-auto-find-the-town-judge-lclighterdiv-classlinerthreadicon-linerfirst-data-highlight-id257713177-data-bundle-id0-idlgt257713177-stylebackground-image-url-and-quot-https-profilegetlinercom-liner-service-bucket-user-photo-default-color-10-wsvg-and-quot-user-select-auto-div-classlinerthreadicon-dim-styleuser-select-auto-div-div-classlinerthreadicon-mentioned-styleuser-select-auto-div-classlinerthreadicon-mentionedimg-styleuser-select-auto-div-div-div-classlinerthreadicon-onlyme-styleuser-select-auto-div-classlinerthreadicon-onlymeimg-styleuser-select-auto-div-div-div.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        
+        vector<int> out(n+1,0),in(n+1,0);
+        
+        for(auto i:trust)
+        {
+            out[i[0]]++;
+            in[i[1]]++;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            if(out[i]==0 && in[i]==n-1)
+            {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
