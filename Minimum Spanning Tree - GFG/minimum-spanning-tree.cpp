0284c90cf061f10e8d1b29b98ea2f8b3a685824b
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-11 16:02:29 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -9,70 +9,45 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-	int pr[1005];
-	
-	int find(int x)
-	{
-	    while(x!=pr[x])
-	    {
-	        x=pr[x];
-	    }
-	    
-	    return x;
-	}
-	
-	void Union(int a,int b)
-	{
-	    int x=find(a);
-	    int y=find(b);
-	    
-	    if(x!=y)
-	    {
-	        pr[y]=x;
-	    }
-	}
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         int n=V;
+        vector<int> key(n,100000);
+        vector<bool> mst(n,false);
         
-        int key[n];
-        bool mst[n];
+         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
+         key[0]=0;
         
-        for(int i=0;i<n;i++)
-        {
-            key[i]=INT_MAX;
-            mst[i]=false;
-            pr[i]=i;
-        }
-        
-        vector<vector<int>> v;
-        
-        for(int i=0;i<n;i++)
-        {
-            for(auto j:adj[i])
-            {
-                v.push_back({j[1],i,j[0]});
-            }
-        }
-        
-        sort(v.begin(),v.end());
-        
-        int ans=0;
-        
-        for(auto i:v)
-        {
-            int w=i[0];
-            int x=i[1];
-            int y=i[2];
-            
-            if(find(x)!=find(y))
-            {
-                Union(x,y);
-                ans+=w;
-            }
-        }
-        
-        return ans;
+         q.push({0,0});
+         
+         while(!q.empty())
+         {
+             auto p=q.top();
+             q.pop();
+             
+             int u=p.second;
+             mst[u]=true;
+             for(auto j:adj[u])
+             {
+                 int v=j[0];
+                 int w=j[1];
+                 
+                 if(mst[v]==false && key[v]>w)
+                 {
+                     key[v]=w;
+                     q.push({key[v],v});
+                 }
+             }
+         }
+         
+         int ans=0;
+         
+         for(int i=0;i<n;i++)
+         {
+             ans+=key[i];
+         }
+         
+         return ans;
     }
 };
 
