93b35030d60971930855067811dbc98a5fad2cd3
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-11 11:22:21 +0530

    Time: 589 ms (21.85%), Space: 38.4 MB (22.98%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,63 +1,46 @@
 class Solution {
 public:
     
-    // vector<vector<string>> ans;
-    int ans=0;
     int dp[605][105][105];
     
-    int solve(int i,int c,int l,vector<string>& s,int m,int n)
+    int solve(vector<string>& a,int i,int m, int n)
     {
-        if(i<0)
+        if(i==a.size())
         {
             return 0;
         }
         
         if(dp[i][m][n]!=-1)
         {
-            return dp[i][m][n];    
+           return dp[i][m][n]; 
         }
-        
-        string a=s[i];
         int c0=0,c1=0;
-        
-        for(int j=0;j<a.size();j++)
+        string s=a[i];
+        for(int j=0;j<s.size();j++)
         {
-              if(a[j]=='0')
-              {
-                  c0++;
-              }
-              if(a[j]=='1')
-              {
-                  c1++;
-              }
+            if(s[j]=='0')
+            {
+                c0++;
+            }
+            else
+            {
+                c1++;
+            }
         }
         
-        int x=0,y=0;
+        int l=0,r=0;
         
-        if(c0<= m && c1<=n)
+        if(c0<=m && c1<=n)
         {
-           x = 1+solve(i-1,c+1,l,s,m-c0,n-c1);
+            l=1+solve(a,i+1,m-c0,n-c1);
         }
+        r=solve(a,i+1,m,n);
         
-        y =solve(i-1,c,l,s,m,n);
-        
-        return dp[i][m][n]=max(x,y);
+        return dp[i][m][n]=max(l,r);
     }
-    
-    int findMaxForm(vector<string>& s, int m, int n) {
-        
-        int l=s.size();
+    int findMaxForm(vector<string>& a, int m, int n) {
         
         memset(dp,-1,sizeof(dp));
-        
-        return solve(l-1,0,l,s,m,n);
-        
-
+        return solve(a,0,m,n);
     }
 };
-
-/*
-["0","11","1000","01","0","101","1","1","1","0","0","0","0","1","0","0110101","0","11","01","00","01111","0011","1","1000","0","11101","1","0","10","0111"]
-9
-80
-*/
