7c7b077c3405f470440fb38496cea60f1bd953df
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-11 10:58:39 +0530

    Time: 18 ms (95.82%), Space: 13.9 MB (62.86%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,58 +1,53 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& p) {
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=numCourses;
         
         vector<int> d(n,0);
-	    vector<int> v[n];
-	    
-	    for(auto i:p)
-	    {
-	        v[i[1]].push_back(i[0]);
-	        d[i[0]]++;
-	    }
-	    
-	    queue<int> q;
-	    
-	    int ans=0;
-	    
-        vector<int> order;
+        vector<int> v[n];
         
-	    for(int i=0;i<n;i++)
-	    {
-	        if(d[i]==0)
-	        {
-	            q.push(i);
-                order.push_back(i);
-	            ans++;
-	        }
-	    }
-	    
-	    while(!q.empty())
-	    {
-	        int k=q.size();
-	        
-	        while(k--)
-	        {
-	            auto p=q.front();
-	            q.pop();
-	            
-	            for(auto x:v[p])
-	            {
-	                d[x]--;
-	                
-	                if(d[x]==0)
-	                {
-	                    q.push(x);
-                        order.push_back(x);
-	                    ans++;
-	                }
-	            }
-	        }
-	    }
-	    
-        vector<int> b;
+        for(auto i:prerequisites)
+        {
+            v[i[1]].push_back(i[0]);
+            d[i[0]]++;
+        }
         
-	    return ans==n ? order : b;
+        queue<int> q;
         
+        for(int i=0;i<n;i++)
+        {
+            if(d[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        
+        int cnt=0;
+        
+        vector<int> ans,emp;
+        
+        while(!q.empty())
+        {
+            int k=q.size();
+            while(k--)
+            {
+                auto p=q.front();
+                q.pop();
+                
+                ans.push_back(p);
+                cnt++;
+                
+                for(auto x:v[p])
+                {
+                    d[x]--;
+                    if(d[x]==0)
+                    {
+                        q.push(x);
+                    }
+                }
+            }
+        }
+        
+        return cnt==n ? ans : emp;
     }
 };
