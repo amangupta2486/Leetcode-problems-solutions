299d1c8fb30e35826350fafbe1e95fdc42562698
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-14 09:51:14 +0530

    Time: 35 ms (44.23%), Space: 10.8 MB (64.11%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,47 +3,29 @@ public:
     
     int dp[1005][1005];
     
-    int solve(string &a, string &b,int i,int j)
+    int solve(string &word1, string &word2,int n,int m)
     {
-        if(i==0 || j==0)
+        if(n==0 ||m==0)
         {
             return 0;
         }
         
-        if(dp[i][j]!=-1)
+        if(dp[n][m]!=-1)
         {
-            return dp[i][j];
+            return dp[n][m];
         }
-        if(a[i-1]==b[j-1])
+        if(word1[n-1]==word2[m-1])
         {
-            return dp[i][j]=1+solve(a,b,i-1,j-1);
+            return dp[n][m]=1+solve(word1,word2,n-1,m-1);
         }
         
-        return dp[i][j]=max(solve(a,b,i,j-1),solve(a,b,i-1,j));
+        return dp[n][m]=max(solve(word1,word2,n-1,m),solve(word1,word2,n,m-1));
     }
-    
-    int longestCommonSubsequence(string a, string b) {
+    int longestCommonSubsequence(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        memset(dp,-1,sizeof(dp));
         
-        int n=a.size();
-        int m=b.size();
-        memset(dp,0,sizeof(dp));
-        
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(a[i-1]==b[j-1])
-                {
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
-        
-        return dp[n][m];
-        //return solve(a,b,n,m);
+        return solve(word1,word2,n,m);
     }
 };
