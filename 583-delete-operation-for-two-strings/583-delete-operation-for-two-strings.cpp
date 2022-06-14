4d44f6dd8879300e62be4bd6a4a3004940cf2c12
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-14 09:36:05 +0530

    Time: 1730 ms (5.02%), Space: 375.9 MB (5.02%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -3,13 +3,12 @@ public:
     
     int dp[505][505];
     
-    int solve(string &a, string &b,int n,int m)
+    int solve(string word1, string word2,int n,int m)
     {
         if(n==0)
         {
             return m;
         }
-        
         if(m==0)
         {
             return n;
@@ -19,19 +18,19 @@ public:
         {
             return dp[n][m];
         }
-        if(a[n-1]==b[m-1])
+        if(word1[n-1]==word2[m-1])
         {
-            return dp[n][m]=solve(a,b,n-1,m-1);
+            return dp[n][m]=solve(word1,word2,n-1,m-1);
         }
         
-        return dp[n][m]=1+min(solve(a,b,n-1,m),solve(a,b,n,m-1));
+        return dp[n][m]=1+min(solve(word1,word2,n-1,m),solve(word1,word2,n,m-1));
     }
-    int minDistance(string a, string b) {
+    int minDistance(string word1, string word2) {
         
-        int n=a.size();
-        int m=b.size();
+        int n=word1.size();
+        int m=word2.size();
         memset(dp,-1,sizeof(dp));
         
-        return solve(a,b,n,m);
+        return solve(word1,word2,n,m);
     }
 };
