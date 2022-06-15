93fbe672b80a7c7c2f3518514bcd948005254282
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-15 14:57:34 +0530

    Time: 38 ms (39.14%), Space: 19.3 MB (57.94%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -3,15 +3,15 @@ public:
     void moveZeroes(vector<int>& a) {
         
         int n=a.size();
+        int l=0;
         
-
-        for(int i=0,j=0;i<n && j<n;i++)
+        for(int i=0;i<n;i++)
         {
             if(a[i]!=0)
             {
-                swap(a[j++],a[i]);
+                swap(a[l],a[i]);
+                l++;
             }
         }
-       
     }
 };
