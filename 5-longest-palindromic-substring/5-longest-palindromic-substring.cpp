ce21ccb820f77dff87fd33a4394909c6eb7e35c5
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-16 07:56:10 +0530

    Time: 18 ms (92.68%), Space: 7.1 MB (81.58%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,51 +1,39 @@
 class Solution {
 public:
+    
+    int solve(string &s,int i,int j)
+    {
+        int n=s.size();
+        
+        while(i>=0 && j<n && s[i]==s[j])
+        {
+            i--;
+            j++;
+        }
+        
+        return j-i-1;
+    }
     string longestPalindrome(string s) {
         
         int n=s.size();
-        
-        bool dp[n][n];
-        memset(dp,0,sizeof(dp));
+        int ans=0,st=0,end=0;
         
         for(int i=0;i<n;i++)
         {
-            dp[i][i]=1;
-        }
-        
-        int start=0,len=1;
-        
-        for(int i=0;i<n-1;i++)
-        {
-            if(s[i]==s[i+1])
+            int l=solve(s,i,i);
+            int r=0;
+            if(r<n-1)
+            r=solve(s,i,i+1);
+            
+            int len=max(l,r);
+            //cout<<len<<" ";
+            if(len>ans)
             {
-                start=i;
-                dp[i][i+1]=1;
-                len=2;
+                st=i-((len-1)/2);
+                ans=len;
             }
         }
         
-        for(int k=3;k<=n;k++)
-        {
-            for(int i=0;i<n-k+1;i++)
-            {
-                int j=i+k-1;
-                
-                if(s[i]==s[j] && dp[i+1][j-1])
-                {
-                    dp[i][j]=1;
-                    start=i;
-                    len=max(len,k);
-                }
-            }
-        }
-        
-        string ans="";
-       // cout<<len;
-        for(int i=start;i<start+len;i++)
-        {
-            ans+=s[i];
-        }
-        
-        return ans;
+        return s.substr(st,ans);
     }
 };
