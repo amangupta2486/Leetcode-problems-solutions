ead39cca259e553f22d2fcf2f8c7dbbdf89c2faa
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-16 07:43:48 +0530

    Time: 227 ms (48.32%), Space: 8.4 MB (63.49%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,46 +1,51 @@
 class Solution {
 public:
-    
-    int solve(string s,int i,int j)
-    {
-        int n=s.size();
-        
-        while(i>=0 && j<n && s[i]==s[j])
-        {
-            i--;
-            j++;
-        }
-        
-        return j-i-1;
-    }
     string longestPalindrome(string s) {
         
-        int start=0,end=0,ans=1;
         int n=s.size();
         
-        if(n==0)
-        {
-            return "";
-        }
+        bool dp[n][n];
+        memset(dp,0,sizeof(dp));
         
         for(int i=0;i<n;i++)
-        {   
-            int l1=solve(s,i,i);
-            int l2=solve(s,i,i+1);
-
-            int l=max(l1,l2);
-
-            if(l>ans)
-            {
-                ans=l;
-                
-                start = i-(l-1)/2;
-                end   = i+(l)/2;
-            }
-
+        {
+            dp[i][i]=1;
         }
-        cout<<ans<<endl;
-        return s.substr(start,end-start+1);
+        
+        int start=0,len=1;
+        
+        for(int i=0;i<n-1;i++)
+        {
+            if(s[i]==s[i+1])
+            {
+                start=i;
+                dp[i][i+1]=1;
+                len=2;
+            }
+        }
+        
+        for(int k=3;k<=n;k++)
+        {
+            for(int i=0;i<n-k+1;i++)
+            {
+                int j=i+k-1;
+                
+                if(s[i]==s[j] && dp[i+1][j-1])
+                {
+                    dp[i][j]=1;
+                    start=i;
+                    len=max(len,k);
+                }
+            }
+        }
+        
+        string ans="";
+       // cout<<len;
+        for(int i=start;i<start+len;i++)
+        {
+            ans+=s[i];
+        }
+        
+        return ans;
     }
-    
 };
