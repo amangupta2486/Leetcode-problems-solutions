8a12a794377eb0e34b2c6a25b28e97ff4b4ed738
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-17 20:07:34 +0530

    Time: 299 ms (82.46%), Space: 8.3 MB (6.79%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -4,11 +4,12 @@ public:
     int dx[4]={1,-1,0,0};
     int dy[4]={0,0,1,-1};
     int ans=0;
-    int vis[10][10];
-    void solve(vector<vector<char>>& b, string w,int i,int j,int n,int m,int z)
+    
+    void solve(int i,int j,vector<vector<char>>& b, string &w,int c,vector<vector<int>> &vis)
     {
-        if(z==w.size())
+        if(c==w.size())
         {
+            //cout<<c<<" ";
             ans=1;
             return;
         }
@@ -18,34 +19,39 @@ public:
             int x=i+dx[k];
             int y=j+dy[k];
             
-            if(x>=0 && x<n && y>=0 && y<m && b[x][y]==w[z] && !vis[x][y])
+            if(x>=0 && x<b.size() && y>=0 && y<b[0].size() && b[x][y]==w[c] && !vis[x][y])
             {
-                //cout<<w[z];
                 vis[x][y]=1;
-                solve(b,w,x,y,n,m,z+1);
+                solve(x,y,b,w,c+1,vis);
                 vis[x][y]=0;
             }
         }
+
     }
+    
     bool exist(vector<vector<char>>& b, string w) {
-        
+        ans=0;
         int n=b.size();
         int m=b[0].size();
- 
+        
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(b[i][j]==w[0])
                 {
-                    memset(vis,0,sizeof(vis));
+                    vector<vector<int>> vis(n,vector<int>(m,0));
                     vis[i][j]=1;
-                    solve(b,w,i,j,n,m,1);
-                  //cout<<endl;
+                    solve(i,j,b,w,1,vis);
+                    
+                    if(ans)
+                    {
+                        return 1;
+                    }
                 }
             }
         }
         
-        return ans;
+        return 0;
     }
 };
