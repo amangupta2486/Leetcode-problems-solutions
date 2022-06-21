d4290129d8fe13f13e286bdb576b51b6690f89ba
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-21 15:53:55 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (41.12%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -3,31 +3,34 @@ public:
     int rob(vector<int>& a) {
         
         int n=a.size();
-	
+        
         if(n<4)
         {
             return *max_element(a.begin(),a.end());
         }
-
-        vector<long long int> b(n,0);
-        vector<long long int> d(n,0);
-
-        b[0]=a[0];
-        b[1]=max(a[0],a[1]);
-
+        
+        vector<int> dp(n,0);
+        dp[0]=a[0];
+        dp[1]=max(a[0],a[1]);
+        
         for(int i=2;i<n-1;i++)
         {
-            b[i]=max(a[i]+b[i-2],b[i-1]);
+            dp[i]=max(dp[i-2]+a[i],dp[i-1]);
         }
-
-        d[1]=a[1];
-        d[2]=max(a[1],a[2]);
-
+        
+        int p1=*max_element(dp.begin(),dp.end());
+        
+        vector<int> dp1(n,0);
+        dp1[1]=a[1];
+        dp1[2]=max(a[2],a[1]);
+        
         for(int i=3;i<n;i++)
         {
-            d[i]=max(a[i]+d[i-2],d[i-1]);
+            dp1[i]=max(dp1[i-2]+a[i],dp1[i-1]);
         }
-
-        return max(b[n-2],d[n-1]);
+        
+        int p2=*max_element(dp1.begin(),dp1.end());
+        
+        return max({p1,p2});
     }
 };
