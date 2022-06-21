3497008af15221b4c452464681bfc91f7104c2dd
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-21 16:35:50 +0530

    Time: 23 ms (7.60%), Space: 20.6 MB (5.23%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
-    map<int,int> mp;
-    int dp[400];
+    unordered_map<int,int> mp;
     
-    int solve(int i,vector<int>& c)
+    int dp[366];
+    
+    int solve(int i,vector<int>& days, vector<int>& costs)
     {
         if(i>365)
         {
             return 0;
         }
         
-        if(dp[i]!=0)
+        if(dp[i]!=-1)
         {
             return dp[i];
         }
-
         int ans=0;
         
-        if(mp[i]>0)
+        if(mp[i])
         {
-            ans =min({c[0]+solve(i+1,c),c[1]+solve(i+7,c),c[2]+solve(i+30,c)});
+            ans=min({costs[0]+solve(i+1,days,costs),costs[1]+solve(i+7,days,costs),costs[2]+solve(i+30,days,costs)});
         }
         else
         {
-            ans = solve(i+1,c);
+            ans=solve(i+1,days,costs);
         }
         
         return dp[i]=ans;
     }
-    int mincostTickets(vector<int>& d, vector<int>& c) {
+    
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
         
-        int n=d.size();
-        
-        for(int i=0;i<n;i++)
+        for(auto i:days)
         {
-            mp[d[i]]++;
+            mp[i]++;
         }
-        memset(dp,0,sizeof(dp));
         
-        return solve(1,c);
+        memset(dp,-1,sizeof(dp));
+        
+        return solve(0,days,costs);
     }
 };
