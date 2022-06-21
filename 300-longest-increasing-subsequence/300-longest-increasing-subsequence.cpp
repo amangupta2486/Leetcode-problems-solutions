dfbb3d1a0faa05de5a287f46ac38dae7a71390fa
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-21 10:20:28 +0530

    Time: 410 ms (38.70%), Space: 10.5 MB (66.31%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -6,13 +6,13 @@ public:
         
         vector<int> dp(n,1);
         
-        for(int i=1;i<n;i++)
+        for(int i=0;i<n;i++)
         {
             for(int j=0;j<i;j++)
             {
-                if(a[j]<a[i] && dp[j]+1>dp[i])
+                if(a[j]<a[i] && dp[i]<dp[j]+1)
                 {
-                    dp[i]=1+dp[j];
+                    dp[i]=dp[j]+1;
                 }
             }
         }
@@ -20,3 +20,11 @@ public:
         return *max_element(dp.begin(),dp.end());
     }
 };
+/*
+Input
+[1,3,6,7,9,4,10,5,6]
+Output
+5
+Expected
+6
+*/
