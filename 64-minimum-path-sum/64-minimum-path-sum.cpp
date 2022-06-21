5cf4ceb0feef79bdbc82bd611238c33f0a5a6c79
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-21 15:32:09 +0530

    Time: 19 ms (25.63%), Space: 10 MB (59.12%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     
+    int n,m;
+    
     int dp[205][205];
     
-    int solve(vector<vector<int>>& g,int i,int j)
+    int solve(int i,int j,vector<vector<int>>& g)
     {
-        if(i>=g.size() || j>=g[0].size())
-        {
-            return INT_MAX;
-        }
         
-        if(i==g.size()-1 && j==g[0].size()-1)
+        if(i==n-1 && j==m-1)
         {
             return g[i][j];
+        }
+        
+       
+        if(i>=n || j>=m)
+        {
+            return 100000;
         }
         
         if(dp[i][j]!=-1)
         {
             return dp[i][j];
         }
-        return dp[i][j]=g[i][j]+min(solve(g,i+1,j),solve(g,i,j+1));
+        
+        return dp[i][j]=g[i][j]+min({solve(i+1,j,g),solve(i,j+1,g)});
     }
     
     int minPathSum(vector<vector<int>>& g) {
         
+        n=g.size();
+        m=g[0].size();
+        
         memset(dp,-1,sizeof(dp));
-        return solve(g,0,0);
+        
+        return solve(0,0,g);
     }
 };
