dac79fab354aa74e1007916531d6c02eb6e05a92
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-21 16:05:15 +0530

    Time: 21 ms (56.20%), Space: 13.6 MB (52.61%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -4,6 +4,7 @@ public:
         
         int n=a.size();
         unordered_map<int,int> mp;
+        
         int mx=0;
         
         for(int i=0;i<n;i++)
@@ -14,12 +15,13 @@ public:
         
         vector<int> dp(mx+1,0);
         
-        dp[0]=0;
         dp[1]=mp[1];
+        if(mx>1)
+        dp[2]=max(dp[1],mp[2]*2);
         
-        for(int i=2;i<=mx;i++)
+        for(int i=3;i<=mx;i++)
         {
-            dp[i]=max(dp[i-2]+(mp[i]*i),dp[i-1]);
+            dp[i]=max(dp[i-1],dp[i-2]+(mp[i]*i));
         }
         
         return dp[mx];
