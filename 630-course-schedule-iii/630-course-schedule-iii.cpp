f250d3fa403502749fe7cf5001057f6aace09bd0
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-23 21:18:51 +0530

    Time: 553 ms (18.36%), Space: 56.2 MB (58.52%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,36 +1,79 @@
 class Solution {
 public:
-    int scheduleCourse(vector<vector<int>>& c) {
+    int scheduleCourse(vector<vector<int>>& a) {
         
-        int n=c.size();
+//         sort(a.begin(),a.end());
         
-        int ans=0;
-        
-        sort(c.begin(),c.end(),[&](auto &a,auto &b){
+        sort(a.begin(),a.end(),[&](auto &a,auto &b){
             return a[1]<b[1];
         });
         
-        priority_queue<int> p;
+        // for(auto i:a)
+        // {
+        //     cout<<i[0]<<","<<i[1]<<" ";
+        // }
         
-        int time=0;
+       // cout<<endl;
+        priority_queue<int> q;
+        
+        int ans=0,c=0;
+        int n=a.size();
         
         for(int i=0;i<n;i++)
         {
-            if(time+c[i][0]<=c[i][1])
-            {
-                time+=c[i][0];
-                ans++;
-                p.push(c[i][0]);
-            }
+          if(a[i][0]>a[i][1])
+          {
+              continue;
+          }
             
-            else if(!p.empty() && p.top()>c[i][0])
-            {
-                time+=c[i][0]-p.top();
-                p.pop();
-                p.push(c[i][0]);
-            }
+          if(ans+a[i][0]<=a[i][1])
+          {
+              q.push(a[i][0]);
+              ans+=a[i][0];
+              c++;
+          }
+          else if(!q.empty())
+          {
+              
+              if(q.top()>a[i][0])
+              {
+                  ans-=q.top();
+                  q.pop();
+                  c--;
+              
+              }
+              
+              if(ans+a[i][0]<=a[i][1])
+              {
+                  ans+=a[i][0];
+                  q.push(a[i][0]);
+                  c++;
+              }
+              
+          }
         }
         
-        return ans;
+        // while(!q.empty())
+        // {
+        //     cout<<q.top()<<endl;
+        //     q.pop();
+        // }
+        return c;
     }
 };
+
+/*
+[[100,200],[200,1300],[1000,1250],[2000,3200]]
+[[1,2]]
+[[3,2],[4,3]]
+[[5,5],[4,6],[2,6]]
+[[2,5],[2,19],[1,8],[1,3]]
+[[7,16],[2,3],[3,12],[3,14],[10,19],[10,16],[6,8],[6,11],[3,13],[6,16]]
+
+Input
+[[5,5],[4,6],[2,6]]
+Output
+1
+Expected
+2
+*/
