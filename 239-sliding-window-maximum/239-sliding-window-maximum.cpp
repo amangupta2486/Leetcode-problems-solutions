f7bd7891a83d07d1571caf4fc5596dcc5d9c1cf9
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-24 11:14:35 +0530

    Time: 499 ms (36.71%), Space: 134.7 MB (63.80%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -14,29 +14,27 @@ public:
             {
                 q.pop_back();
             }
-            q.push_back(i);
-        }
-        
-         //ans.push_back(a[q.front()]);
-        
-        for(int i=k;i<n;i++)
-        {
-            ans.push_back(a[q.front()]);
-            
-            while(!q.empty() && q.front()<=i-k)
-            {
-                q.pop_front();
-            }
-            
-            while(!q.empty() && a[q.back()]<a[i])
-            {
-                q.pop_back();
-            }
             
             q.push_back(i);
         }
         
         ans.push_back(a[q.front()]);
+        
+        for(int i=k;i<n;i++)
+        {
+            while(!q.empty() && q.front()<=i-k)
+            {
+                q.pop_front();
+            }
+            while(!q.empty() && a[q.back()]<=a[i])
+            {
+                q.pop_back();
+            }
+            
+            q.push_back(i);
+            
+            ans.push_back(a[q.front()]);
+        }
         
         return ans;
     }
