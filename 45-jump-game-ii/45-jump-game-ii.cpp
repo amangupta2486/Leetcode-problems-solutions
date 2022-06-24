2dd789f20e6535a1daa6856a7776d66176fd507b
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-24 19:17:14 +0530

    Time: 451 ms (29.56%), Space: 17.6 MB (15.32%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
+    
+    int dp[10005];
+    
+    int solve(int i,int n,vector<int>& a)
+    {
+        if(i>=n-1)
+        {
+            return 0;
+        }
+        
+        if(dp[i]!=-1)
+        {
+            return dp[i];
+        }
+        
+        int ans=INT_MAX;
+        
+        if(a[i]==0)
+        {
+            return 10000;
+        }
+        
+        for(int j=1;j<=a[i];j++)
+        {
+            ans=min(ans,solve(i+j,n,a));
+        }
+        
+        return dp[i]=1+ans;
+    }
+    
     int jump(vector<int>& a) {
         
         int n=a.size();
-        int j=0,end=0,far=0;
         
-        for(int i=0;i<n-1;i++)
-        {
-            far=max(far,a[i]+i);
-            
-            if(i==end)
-            {
-                end=far;
-                j++;
-            }
-        }
+        memset(dp,-1,sizeof(dp));
         
-        return j;
+        return solve(0,n,a);
     }
 };
