3067c696df4542c1a40b022f95b55174e2ddd15f
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-27 13:35:08 +0530

    Time: 0 ms (100.00%), Space: 6 MB (73.02%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -5,14 +5,14 @@ public:
     
     int solve(int i,int j,int n,int m)
     {
-        if(i==n-1 && j==m-1)
-        {
-            return 1;
-        }
-        
         if(i>=n || j>=m)
         {
             return 0;
+        }
+        
+        if(i==n-1 && j==m-1)
+        {
+            return 1;
         }
         
         if(dp[i][j]!=-1)
@@ -20,7 +20,7 @@ public:
             return dp[i][j];
         }
         
-        return dp[i][j]=solve(i+1,j,n,m)+ solve(i,j+1,n,m);
+        return dp[i][j]=solve(i+1,j,n,m) + solve(i,j+1,n,m);
     }
     int uniquePaths(int n, int m) {
         
