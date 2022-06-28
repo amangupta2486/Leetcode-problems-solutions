e3216378b71b3f7588e453ac415f19669e453bb2
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-28 11:05:43 +0530

    Time: 11 ms (77.22%), Space: 14.4 MB (16.43%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        
-        int givencol=image[sr][sc];
-        
-        if(givencol==newColor)
-        {
-            return image;    
-        }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
         int n=image.size();
         int m=image[0].size();
         
+        int currcol=image[sr][sc];
+        
+        if(currcol==color)
+        {
+            return image;
+        }
+        
+        int dx[4]={1,-1,0,0};
+        int dy[4]={0,0,1,-1};
+        
         queue<vector<int>> q;
-        image[sr][sc]=newColor;
+        
+        image[sr][sc]=color;
         
         q.push({sr,sc});
         
-        int dx[4]={1,-1,0,0};
-        int dy[4]={0,0 ,1,-1};
-        
         while(!q.empty())
         {
-            int k=q.size();
+            int l=q.size();
             
-            while(k--)
+            while(l--)
             {
                 auto p=q.front();
                 q.pop();
@@ -37,12 +38,13 @@ public:
                     int x=i+dx[k];
                     int y=j+dy[k];
                     
-                    if(x>=0 && x<n && y>=0 && y<m && image[x][y]==givencol)
+                    if(x>=0 && x<n && y>=0 && y<m && image[x][y]==currcol)
                     {
-                        image[x][y]=newColor;
+                        image[x][y]=color;
                         q.push({x,y});
                     }
                 }
+                
             }
         }
         
