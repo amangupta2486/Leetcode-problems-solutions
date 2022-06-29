117c373c4892e3b54d1782a4180b52dd46fb6788
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-29 09:52:18 +0530

    Time: 811 ms (5.08%), Space: 101.9 MB (5.07%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,72 +1,73 @@
 class Solution {
 public:
+    
+    int vis[10005];
+    
+    void dfs(int i,vector<vector<int>> adj[])
+    {
+        vis[i]=1;
+        
+        for(auto j:adj[i])
+        {
+            if(!vis[j[0]])
+            {
+                dfs(j[0],adj);
+            }
+        }
+    }
+    
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
         
-        vector<double> key(n,0);
-        vector<int> vis(n,0);
+        memset(vis,0,sizeof(0));
         
-        key[start]=1;
-        
-        vector<vector<double>> v[n];
-        
-        int i=0;
-        
-        for(auto j:edges)
+        vector<vector<int>> adj[n];
+        vector<double> pro(n,0);
+        for(int i=0;i<edges.size();i++)
         {
-            v[j[0]].push_back({(double)j[1],succProb[i]});
-            v[j[1]].push_back({(double)j[0],succProb[i]});
-            i++;
+            adj[edges[i][0]].push_back({edges[i][1],i});
+            adj[edges[i][1]].push_back({edges[i][0],i});
         }
         
-
+        dfs(start,adj);
+        
+        if(!vis[end])
+        {
+            return 0;
+        }
+        
         priority_queue<pair<double,int>> q;
         
-        
-        vis[start]=1;
         q.push({1,start});
-    
+        
+        double ans=0;
+        pro[start]=1;
+        
         while(!q.empty())
         {
-          auto p=q.top();
-          q.pop();
+            auto p=q.top();
+            q.pop();
             
-          int u=p.second;
-          vis[u]=1;
-          if(u==end)
-          {
-              return p.first;
-          }
-          for(auto j:v[u])
-          {
-              int v=j[0];
-              double w=j[1];
-             //cout<<v<<" "<<w<<endl;
-              if(vis[v]==0 && key[v]<key[u]*w)
-              {
-                  //vis[v]=1;
-                  key[v]=key[u]*w;
-                  q.push({key[u]*w,v});
-              }
-          }
+            double x=p.first;
+            int u=p.second;
+          //  cout<<u<<" "<<x<<endl;
+                
+            if(u==end)
+            {
+                ans=max(ans,x);
+            }
+            for(auto j:adj[u])
+            {
+                auto v=j[0];
+                auto w=succProb[j[1]];
+                
+                if(x*w>pro[v])
+                {
+                    pro[v]=x*w;
+                    q.push({pro[v],v});
+                }
+            }
         }
         
-        // for(int i=0;i<n;i++)
-        // {
-        //     cout<<i<<" "<<key[i]<<endl;
-        // }
-        return 0;
+        return ans;
     }
 };
-
-/*
-Input
-5
-[[1,4],[2,4],[0,4],[0,3],[0,2],[2,3]]
-[0.37,0.17,0.93,0.23,0.39,0.04]
-3
-4
-Output
-0.00000
-Expected
-0.21390
-*/
