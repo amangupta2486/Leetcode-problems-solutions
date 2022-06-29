7e84f36001b5f914341f046da89ca47fad8770b2
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-29 10:44:10 +0530

    Time: 198 ms (91.75%), Space: 84.2 MB (34.41%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -3,25 +3,30 @@ public:
     vector<int> canSeePersonsCount(vector<int>& h) {
         
         int n=h.size();
-        stack<int> s;
-        vector<int> ans(n,0);
         
+        stack<int> s;
+        vector<int> v(n,0);
+
         for(int i=n-1;i>=0;i--)
         {
+            int c=0;
+            
             while(!s.empty() && s.top()<h[i])
             {
-                ans[i]++;
+                c++;
                 s.pop();
             }
+
+            v[i]+=c;
             
-            if(s.size()>0)
+            if(!s.empty())
             {
-                ans[i]++;
+                v[i]+=1;
             }
             
             s.push(h[i]);
         }
         
-        return ans;
+        return v;
     }
 };
