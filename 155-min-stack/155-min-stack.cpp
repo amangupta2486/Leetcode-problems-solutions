1ba7be1f6a1688e9e0505bceca2a10e9595b886d
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-06-30 16:13:27 +0530

    Time: 30 ms (64.77%), Space: 16.3 MB (69.14%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,24 +1,25 @@
 class MinStack {
 public:
+    
     stack<pair<int,int>> s;
-
+    
     MinStack() {
         
     }
     
     void push(int val) {
         
-        int min;
         if(s.empty())
         {
-            min=val;
-        }
-        else
-        {
-            min=std::min(s.top().second,val);
+            s.push({val,val});
         }
         
-        s.push({val,min});
+        else
+        {
+            auto p=s.top();
+            int mi=min(val,p.first);
+            s.push({mi,val});
+        }
     }
     
     void pop() {
@@ -26,15 +27,15 @@ public:
     }
     
     int top() {
+        
         auto p=s.top();
-
-        return p.first;
+        return p.second;
     }
     
     int getMin() {
+        
         auto p=s.top();
-         
-        return p.second;
+        return p.first;
     }
 };
 
