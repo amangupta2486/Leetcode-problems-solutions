bf5e05fbe7240daa4527973d6dfb56a57026ca4b
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-07-02 11:28:44 +0530

    Time: 243 ms (58.98%), Space: 17.9 MB (46.29%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,54 +1,55 @@
 class Solution {
 public:
-    
-    int ans=0;
-    int dp[205][20005];
-    
-    bool solve(int i,int n,int s,int t,vector<int>& a)
-    {
-        if(i<0 || s==t)
-        {
-            if(s==t)
-            {
-                return 1;
-            }
-            return 0;
-        }
-        
-        if(dp[i][s]!=-1)
-        {
-            return dp[i][s];
-        }
-        int l=0,r=0;
-        if(a[i]+s<=t)
-        {
-            l=solve(i-1,n,s+a[i],t,a);
-        }
-        
-        r= solve(i-1,n,s,t,a);
-        
-        return dp[i][s] = l || r;
-    }
     bool canPartition(vector<int>& a) {
         
-        int s=0;
         int n=a.size();
+        
+        int s=0;
         
         for(int i=0;i<n;i++)
         {
             s+=a[i];
         }
         
-        if(s&1)
+        if(s%2==1)
         {
             return 0;
         }
         
         s=s/2;
         
-        memset(dp,-1,sizeof(dp));
-        return solve(n-1,n,0,s,a);
+        int dp[n+1][s+1];
+        memset(dp,0,sizeof(dp));
         
-        //return ans;
+        dp[0][0]=0;
+        
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=1;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=s;j++)
+            {
+                if(a[i-1]<=j)
+                {
+                    dp[i][j]=dp[i-1][j-a[i-1]] || dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        
+        // for(int j=0;j<=s;j++)
+        // {
+        //     cout<<dp[n][j]<<" ";
+        // }
+        // cout<<endl;
+        return dp[n][s];
     }
 };
+
+//[1,2,5]
