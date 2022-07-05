5e16fb20c1c7405df9fef5b9163eca63ffab2486
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-07-05 18:03:11 +0530

    Time: 197 ms (14.09%), Space: 48.3 MB (7.48%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -4,23 +4,34 @@ public:
         
         int n=a.size();
         
-        map<int,int> m;
-        m[0]=1;
+        vector<int> p(n+1,0);
         
-        int s=0,ans=0;
-        
-        for(int i=0;i<n;i++)
+        for(int i=1;i<=n;i++)
         {
-            s+=a[i];
-            
-            if(m[s-k])
-            {
-                ans+=m[s-k];
-            }
-            
-            m[s]++;
+            p[i]=p[i-1]+a[i-1];
         }
         
-        return ans;
+        int cnt=0;
+        
+        unordered_map<int,int> mp;
+        
+        for(int i=1;i<=n;i++)
+        {
+            int x=p[i]-k;
+            
+            if(p[i]==k)
+            {
+                cnt++;
+            }
+            
+            if(mp[x]>0)
+            {
+                cnt+=mp[x];
+            }
+            
+            mp[p[i]]++;
+        }
+        
+        return cnt;
     }
 };
