16a9f5f619d893c1d9e3842c4dd6dae0b7647e45
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-07-07 15:15:40 +0530

    Time: 61 ms (11.77%), Space: 15.1 MB (11.55%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     
-    vector<int> v[205];
+    vector<int> adj[205];
     int vis[205];
+    
     
     void dfs(int i)
     {
         vis[i]=1;
         
-        for(auto j:v[i])
+        for(auto j:adj[i])
         {
             if(!vis[j])
             {
@@ -16,36 +17,37 @@ public:
             }
         }
     }
-    int findCircleNum(vector<vector<int>>& g) {
-        
+    int findCircleNum(vector<vector<int>>& isConnected) {
+    
         memset(vis,0,sizeof(vis));
         
-        int n=g.size();
-        int m=g[0].size();
+        int n=isConnected.size();
+        int m=isConnected[0].size();
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(g[i][j]==1)
+                if(isConnected[i][j]==1)
                 {
-                    v[i].push_back(j);
-                    v[j].push_back(i);
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
         
-        int c=0;
+        int ans=0;
         
         for(int i=0;i<n;i++)
         {
             if(!vis[i])
             {
                 dfs(i);
-                c++;
+                ans++;
             }
         }
         
-        return c;
+        return ans;
+        
     }
 };
