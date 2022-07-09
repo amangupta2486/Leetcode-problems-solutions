e7f88db0210316819d7184706a430d4c94e48382
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-07-09 11:15:55 +0530

    Time: 227 ms (74.09%), Space: 88.7 MB (46.59%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,65 +1,39 @@
 class Solution {
 public:
-    /*
-    int dp[100005][2][3];
+    int dp[100005][3][2];
     
-    int solve(int i,vector<int>& p,int buy,int k)
+    int solve(int i,int buy,int n,vector<int>& p,int k)
     {
-        if(i>=p.size() || k==0)
+        if(i>=n || k==0)
         {
             return 0;
         }
         
-        if(dp[i][buy][k]!=-1)
+        if(dp[i][k][buy]!=-1)
         {
-            return dp[i][buy][k];
+            return dp[i][k][buy];
         }
-        int profit=0;
+        
+        int ans=0;
         
         if(buy)
         {
-            profit=max(-1*p[i]+solve(i+1,p,0,k),solve(i+1,p,1,k));
+            ans=max(-p[i]+solve(i+1,0,n,p,k),solve(i+1,1,n,p,k));
         }
         else
         {
-            profit=max(p[i]+solve(i+1,p,1,k-1),solve(i+1,p,0,k));
+            ans=max(p[i]+solve(i+1,1,n,p,k-1),solve(i+1,0,n,p,k));
         }
         
-        return dp[i][buy][k]=profit;
+        return dp[i][k][buy]=ans;
     }
-    */
+    
     int maxProfit(vector<int>& p) {
         
         int n=p.size();
-        // int dp[n+1][2];
-        // int after[2][3],curr[2][3];
-        // memset(after,0,sizeof(after));
-        // memset(curr,0,sizeof(curr));
         
-        vector<vector<int>> after(2,vector<int>(3,0)),curr(2,vector<int>(3,0));
-        //return solve(0,p,1,2);
+        memset(dp,-1,sizeof(dp));
         
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int buy=0;buy<2;buy++)
-            {
-                for(int k=1;k<=2;k++)
-                {
-                    int profit=0;
-                    if(buy)
-                    {
-                        profit=max(-p[i]+after[0][k],after[1][k]);
-                    }
-                    else
-                    {
-                        profit=max(p[i]+after[1][k-1],after[0][k]);
-                    }
-                    curr[buy][k]=profit;
-                }
-                after=curr;
-            }
-        }
-        
-        return after[1][2];
+        return solve(0,1,n,p,2);
     }
 };
