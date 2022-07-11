b59068cff76d0065a63360a59b21809dbc866da5
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-07-11 22:23:40 +0530

    Time: 461 ms (63.13%), Space: 116.8 MB (98.58%) - LeetHub

---
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -0,0 +1,101 @@
+class MedianFinder {
+public:
+    
+    priority_queue<int> pq1;
+    priority_queue<int,vector<int>,greater<int>> pq2;
+    
+    int n;
+    
+    MedianFinder() {
+        n=0;  
+    }
+    
+    void addNum(int num) {
+        
+        if(pq1.empty())
+        {
+            pq1.push(num);
+            n++;
+            return ;
+        }
+        
+         n++;
+        
+        if(pq1.top()>=num)
+        {
+            pq1.push(num);
+        }
+        else
+        {
+            pq2.push(num);
+        }
+        
+        int x=pq1.size();
+        int y=pq2.size();
+        
+        
+        if(n%2==0)
+        {
+            while(x>y)
+            {
+                pq2.push(pq1.top());
+                pq1.pop();
+                x--;
+                y++;
+            }
+            
+            while(y>x)
+            {
+                pq1.push(pq2.top());
+                pq2.pop();
+                x++;
+                y--;
+            }
+        }
+        
+        else
+        {
+            int c=(n+1)/2;
+            
+            while(x<c)
+            {
+                pq1.push(pq2.top());
+                pq2.pop();
+                x++;
+                y--;
+            }
+            
+            while(x>c)
+            {
+                pq2.push(pq1.top());
+                pq1.pop();
+                x--;
+                y++;
+            }
+        }
+        
+    }
+    
+    double findMedian() {
+        
+        double x;
+        
+        if(n%2==0)
+        {
+            x=(pq1.top()+pq2.top())/2.0;
+        }
+        else
+        {
+            x=pq1.top();
+        }
+        
+        return x;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
