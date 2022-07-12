35f6d870363334ce4d1858b97bcc3eeb0111e1a9
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-07-12 15:32:25 +0530

    Time: 66 ms (98.89%), Space: 23.9 MB (55.77%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -5,57 +5,6 @@ public:
         int n=g.size();
         int m=g[0].size();
         
-        /*
-        int s=min(n,m);
-        
-        int ans=0;
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(g[i][j]==1)
-                {
-                    int k=1;
-                    int x=i;
-                    int y=j;
-                    
-                    int f=1;
-                    ans++;
-                    
-                    while(x<n-k && y<m-k && f)
-                    {
-                        for(int a=y;a<=y+k;a++)
-                        {
-                            if(g[x+k][a]!=1)
-                            {
-                                f=0;
-                                break;
-                            }
-                        }
-                        
-                        for(int b=x;b<=x+k;b++)
-                        {
-                            if(g[b][y+k]!=1)
-                            {
-                                f=0;
-                                break;
-                            }
-                        }
-                        
-                        if(f)
-                        {
-                            ans++;
-                            k++;
-                        }
-                    }
-                }
-            }
-        }
-        
-        return ans;
-        */
-        
         int dp[n+1][m+1];
         memset(dp,0,sizeof(dp));
         
@@ -67,19 +16,13 @@ public:
             {
                 if(g[i-1][j-1]==1)
                 {
-                    ans++;
                     dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
                 }
                 
-                if(dp[i][j]>1)
-                {
-                    ans+=dp[i][j]-1;
-                }
-                
-            }
+                ans+=dp[i][j];
+            }    
         }
         
         return ans;
-        
     }
 };
