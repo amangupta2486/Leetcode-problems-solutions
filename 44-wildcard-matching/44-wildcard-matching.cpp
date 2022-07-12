fbd010ab1e8d4576338361da7d4e285366d0aa3f
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-07-12 15:48:35 +0530

    Time: 62 ms (71.42%), Space: 11 MB (68.85%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -10,11 +10,11 @@ public:
         
         dp[0][0]=1;
         
-        for(int i=1;i<=m;i++)
+        for(int j=1;j<=m;j++)
         {
-            if(p[i-1]=='*')
+            if(p[j-1]=='*')
             {
-                dp[0][i]=dp[0][i-1];
+                dp[0][j]=dp[0][j-1];
             }
         }
         
@@ -27,7 +27,7 @@ public:
                     dp[i][j]=dp[i-1][j-1];
                 }
                 
-                if(p[j-1]=='*')
+                else if(p[j-1]=='*')
                 {
                     dp[i][j]=dp[i-1][j] || dp[i][j-1];
                 }
