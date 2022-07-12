1e4e261e827eb211bd5ce4b1b9a7baa03267e773
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-07-12 11:17:18 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -10,30 +10,61 @@ using namespace std;
 
 class Solution{
   public:
-    int cutRod(int val[], int n) {
+  
+  int solve(int i,int n,int *a,int s,int t,vector<vector<int>> &dp)
+	{
+	    if(i==n || s==t)
+	    {
+	        if(s==t)
+	        {
+	            return 1;
+	        }
+	        return 0;
+	    }
+	    
+	    if(dp[i][s]!=-1)
+	    {
+	        return dp[i][s];
+	    }
+	    
+	    int l=0,r=0;
+	    
+	    if(i+1+s<=t)
+	    {
+	        l = max(a[i]+solve(i,n,a,s+i+1,t,dp) , solve(i+1,n,a,s,t,dp));
+	    }
+	    
+	    else
+	    l=solve(i+1,n,a,s,t,dp);
+	    
+	    return dp[i][s]=l;
+	}
+    int cutRod(int p[], int n) {
+        //code here
+        //vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+
+	    //return solve(0,n,p,0,n,dp);
         
-        int w=n;
-        
-        int dp[n+1][w+1];
+
+        int dp[n+1][n+1];
         memset(dp,0,sizeof(dp));
         
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=w;j++)
+            for(int j=1;j<=n;j++)
             {
                 if(i<=j)
                 {
-                    dp[i][j]= max(val[i-1]+dp[i][j-i],dp[i-1][j]);
+                    dp[i][j]=max(p[i-1]+dp[i][j-i],dp[i-1][j]);
                 }
                 else
                 {
-                     dp[i][j]= dp[i-1][j];
+                    dp[i][j]=dp[i-1][j];
                 }
-                
             }
         }
         
-        return dp[n][w];
+        return dp[n][n];
     }
 };
 
