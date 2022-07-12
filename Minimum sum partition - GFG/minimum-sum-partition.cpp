79ec919d44042339680fa3e4d608389eda8f3263
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-07-12 12:13:36 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -7,49 +7,55 @@ class Solution{
 
   public:
 	int minDifference(int a[], int n)  { 
+	    // Your code goes here
+	    /* 
+	        s2-s1=diff
+	        s-s1-s1=diff;
+	        s-2*s1=diff;
+	    */
 	    
-	    int s=0;
+	    int s=0,S=0;
+	    
 	    for(int i=0;i<n;i++)
-        {
-            s+=a[i];
-        }
-        int sum=s;
-        
-        bool dp[n+1][s+1];
-        
-        memset(dp,0,sizeof(dp));
-
-        for(int i=0;i<=n;i++)
-        {
-            dp[i][0]=1;
-        }
-        
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=s;j++)
-            {
-                if(a[i-1]<=j)
-                {
-                    dp[i][j]=dp[i-1][j] || dp[i-1][j-a[i-1]];
-                }
-                else
-                {
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        
-        int mx=0;
-        
-        for(int j=1;j<=s/2;j++)
-        {
-            if(dp[n][j])
-            {
-                mx=max(mx,j);
-            }
-        }
-        
-        return s-(2*mx);
+	    {
+	        s+=a[i];
+	    }
+	    
+	    S=s;
+	    //s=s/2;
+	    
+	    bool dp[n+1][s+1];
+	    memset(dp,0,sizeof(dp));
+	    
+	    for(int i=0;i<=n;i++)
+	    dp[i][0]=1;
+	    	    
+	    for(int i=1;i<=n;i++)
+	    {
+	        for(int j=1;j<=s;j++)
+	        {
+	            if(a[i-1]<=j)
+	            {
+	                dp[i][j]= dp[i-1][j-a[i-1]] || dp[i-1][j];
+	            }
+	            else
+	            {
+	                dp[i][j]=dp[i-1][j];
+	            }
+	        }
+	    }
+	    
+	    int mx=0;
+	    
+	    for(int i=1;i<=s/2;i++)
+	    {
+	        if(dp[n][i])
+	        {
+	            mx=max(mx,i);
+	        }
+	    }
+	    //cout<<mx<<" ";
+	    return S-(2*mx);
 	} 
 };
 
