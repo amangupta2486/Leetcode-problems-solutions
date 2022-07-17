a66c84417e533a7358f8a590a9eb561f6770c0b3
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-07-18 00:59:17 +0530

    Time: 39 ms (23.44%), Space: 19.5 MB (92.00%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -3,25 +3,29 @@ public:
     int majorityElement(vector<int>& a) {
         
         int n=a.size();
-        int b=a[0];
-        int c=0;
+        
+        int b=0,c=0;
         
         for(int i=0;i<n;i++)
         {
-            if(b==a[i])
+            if(c==0)
             {
+                b=a[i];
                 c++;
             }
             
-            else if(c==0)
+            else if(a[i]==b)
             {
-                b=a[i];
-                c=1;
+                c++;
             }
             
             else
             {
                 c--;
+                if(c<0)
+                {
+                    c=0;
+                }
             }
         }
         
