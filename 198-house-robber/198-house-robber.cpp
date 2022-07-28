deb30648f65b8869e9b535ec8f9e0669d72b155e
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-07-28 18:42:41 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (47.09%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,25 +3,31 @@ public:
     
     int dp[105];
     
-    int solve(int i,int n,vector<int>& a)
+    int solve(int i,int n,vector<int> &a)
     {
-        if(i<0)
+        if(i>=n)
         {
             return 0;
         }
         
+         
         if(dp[i]!=-1)
         {
             return dp[i];
         }
         
-        return dp[i]=max(a[i]+solve(i-2,n,a),solve(i-1,n,a));
+        int rob=a[i]+solve(i+2,n,a);
+        int not_rob=solve(i+1,n,a);
+       
+        
+        return dp[i]=max(rob,not_rob);
     }
     int rob(vector<int>& a) {
         
         int n=a.size();
+        
         memset(dp,-1,sizeof(dp));
         
-        return solve(n-1,n,a);
+        return solve(0,n,a);
     }
 };
