3d95cef7b15f2b64ca5bf24e83aea3d1d19dbc1f
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-07-29 21:05:30 +0530

    Time: 61 ms (13.86%), Space: 13.4 MB (83.52%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-
-    int vis[105];
+    
+    bool vis[105];
     int col[105];
     
-    bool dfs(int i,int c,vector<vector<int>>& g)
+    bool check(int i,int c,vector<vector<int>>& g)
     {
         vis[i]=1;
         col[i]=c;
@@ -13,21 +13,21 @@ public:
         {
             if(!vis[j])
             {
-                int x=c^1;
+                int x=1^c;
                 
-                if(dfs(j,x,g)==false)
+                if(check(j,x,g))
                 {
-                    return false;
+                    return true;
                 }
             }
             
             else if(col[j]==c)
             {
-                return false;
+                return true;
             }
         }
         
-        return true;
+        return false;
     }
     
     bool isBipartite(vector<vector<int>>& g) {
@@ -36,30 +36,18 @@ public:
         memset(col,0,sizeof(col));
         
         int n=g.size();
-        int m=g[0].size();
-        
-        int f=1;
         
         for(int i=0;i<n;i++)
         {
             if(!vis[i])
             {
-                if(dfs(i,0,g)==false)
+                if(check(i,1,g))
                 {
                     return false;
                 }
             }
-    
         }
-        return f;
+        
+        return true;
     }
 };
-
-/*
-[[1,2,3],[0,2],[0,1,3],[0,2]]
-[[1,3],[0,2],[1,3],[0,2]]
-[[1],[0,3],[3],[1,2]]
-[[1,2,3,4],[0,3,4],[0,3],[0,1,2,4],[0,1,3]]
-[[],[2,4,6],[1,4,8,9],[7,8],[1,2,8,9],[6,9],[1,5,7,8,9],[3,6,9],[2,3,4,6,9],[2,4,5,6,7,8]]
-[[2,3,5,6,7,8,9],[2,3,4,5,6,7,8,9],[0,1,3,4,5,6,7,8,9],[0,1,2,4,5,6,7,8,9],[1,2,3,6,9],[0,1,2,3,7,8,9],[0,1,2,3,4,7,8,9],[0,1,2,3,5,6,8,9],[0,1,2,3,5,6,7],[0,1,2,3,4,5,6,7]]
-*/
