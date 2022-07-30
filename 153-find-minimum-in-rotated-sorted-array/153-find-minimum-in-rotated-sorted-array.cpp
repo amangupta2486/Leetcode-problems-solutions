88a64e443ecf398a863a7f4d606d8cb0fbcf3198
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-07-30 09:54:02 +0530

    Time: 15 ms (7.29%), Space: 10.2 MB (23.09%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,33 +4,32 @@ public:
         
         int n=a.size();
         
-        int i=0,j=n-1;
-        
-        if(a[i]<=a[j])
+        if(a[0]<=a[n-1])
         {
             return a[0];
         }
+        
+        int i=0,j=n-1;
         
         while(i<=j)
         {
             int m=(i+j)/2;
             
-            if(a[m]<=a[(m+1)%n] && a[m]<=a[((m-1)+n)%n])
+            if(a[m]<a[(m+1)%n] && a[m]<a[((m-1)+n)%n])
             {
                 return a[m];
             }
             
             if(a[m]>=a[0])
             {
-                i=m+1;
+                j++;
             }
-            
-            else if(a[m]<=a[n-1])
+            else
             {
-                j=m-1;
+                i--;
             }
         }
         
-        return 0;
+        return a[0];
     }
 };
