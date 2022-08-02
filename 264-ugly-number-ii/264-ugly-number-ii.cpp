372135170bb7d8157bed4cee8c629bf595636fcc
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-08-02 23:42:35 +0530

    Time: 14 ms (68.34%), Space: 5.9 MB (90.80%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     int nthUglyNumber(int n) {
         
-        int c2=0,c3=0,c5=0;
-        
-        vector<int> dp(n,0);
-        
+        int dp[n+2];
         dp[0]=1;
         
-        for(int i=1;i<n;i++)
+        int c2=0,c3=0,c5=0;
+        
+        for(int i=1;i<=n;i++)
         {
-            dp[i]=min({2*dp[c2],3*dp[c3],5*dp[c5]});
-           // cout<<dp[i]<<" "<<2*dp[c2]<<" "<<3*dp[c3]<<" "<<5*dp[c5];
-            if(dp[i]==2*dp[c2])
+            dp[i]=min({dp[c2]*2,dp[c3]*3,dp[c5]*5});
+            
+            if(dp[c2]*2==dp[i])
             {
                 c2++;
             }
-            if(dp[i]==3*dp[c3])
+            if(dp[c3]*3==dp[i])
             {
                 c3++;
             }
-            if(dp[i]==5*dp[c5])
+            if(dp[c5]*5==dp[i])
             {
                 c5++;
             }
@@ -28,9 +27,9 @@ public:
         
         // for(int i=0;i<n;i++)
         // {
-        //    cout<<dp[i]<<" ";
+        //     cout<<dp[i]<<" ";
         // }
-        
+        // cout<<endl;
         return dp[n-1];
     }
 };
