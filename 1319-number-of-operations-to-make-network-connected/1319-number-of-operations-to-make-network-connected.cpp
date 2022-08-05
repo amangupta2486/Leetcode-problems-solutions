8fe0e0d193bca1c5d95ff9225d82c6b37f810140
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-08-05 21:05:27 +0530

    Time: 192 ms (65.40%), Space: 55.1 MB (34.86%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,47 +1,49 @@
 class Solution {
 public:
     
-    vector<int> v[100005];
-    int vis[100005];
-    
-    void dfs(int i)
+    void dfs(int i,vector<int> &vis,vector<int> adj[])
     {
+        
         vis[i]=1;
         
-        for(auto j:v[i])
+        for(auto j:adj[i])
         {
             if(!vis[j])
             {
-                dfs(j);
+                dfs(j,vis,adj);
             }
         }
     }
-    int makeConnected(int n, vector<vector<int>>& c) {
+    
+    int makeConnected(int n, vector<vector<int>>& connections) {
         
-        if(c.size()<n-1)
+        int m=connections.size();
+        
+        if(m<n-1)
         {
             return -1;
         }
         
-        memset(vis,0,sizeof(vis));
+        vector<int> adj[n];
         
-        for(auto i:c)
+        for(auto i:connections)
         {
-            v[i[0]].push_back(i[1]);
-            v[i[1]].push_back(i[0]);
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
         }
         
-        int ans=0;
+        int cc=0;
+        vector<int> vis(n,0);
         
         for(int i=0;i<n;i++)
         {
             if(!vis[i])
             {
-                dfs(i);
-                ans++;
+                dfs(i,vis,adj);
+                cc++;
             }
         }
         
-        return ans-1;
+        return cc-1;
     }
 };
