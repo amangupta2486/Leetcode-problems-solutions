14b7d81d11d9ed9a769f89066709a331b2a38967
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-08-12 01:04:24 +0530

    Time: 48 ms (91.87%), Space: 36.5 MB (22.58%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,46 +1,50 @@
 class Solution {
 public:
     
-    int ans=0;
+    int as=0;
     
-    int dp[50005];
-    int vis[50005];
+    int dp[100005];
+    int vis[100005];
     
     int solve(int i,int n,vector<int>& a)
     {
-        if(i<0 || i>=n)
+        if(a[i]==0)
         {
-            return 0;
+            return 1;
         }
         
+        //cout<<i<<" ";
         
         if(dp[i]!=-1)
         {
             return dp[i];
         }
         
-        if(a[i]==0)
+        int ans=0;
+        
+        if(i+a[i]<n && vis[i+a[i]]==0)
         {
-           return dp[i]=1;
+            vis[i+a[i]]=1;
+            ans|=solve(i+a[i],n,a);
+            
         }
         
-        if(vis[i])
+        if(i-a[i]>=0 && vis[i-a[i]]==0)
         {
-            return 0;
+            vis[i-a[i]]=1;
+           ans|=solve(i-a[i],n,a);
         }
         
-        vis[i]=1;
-
-        return dp[i]= solve(i+a[i],n,a) || solve(i-a[i],n,a);
+        return dp[i]=ans;
     }
-    bool canReach(vector<int>& a, int s) {
+    
+    bool canReach(vector<int>& a, int st) {
         
         int n=a.size();
         
         memset(dp,-1,sizeof(dp));
         memset(vis,0,sizeof(dp));
-        return solve(s,n,a);
         
-        //return ans;
+        return solve(st,n,a);
     }
 };
