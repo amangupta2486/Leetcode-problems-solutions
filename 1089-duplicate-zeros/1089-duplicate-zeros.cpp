7a87b45dfd34aae78a5cec39afc7a47eb52b92cf
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-08-11 13:00:46 +0530

    Time: 147 ms (14.25%), Space: 9.6 MB (93.89%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -8,14 +8,18 @@ public:
         {
             if(a[i]==0)
             {
-                a.insert(a.begin()+i+1,0);
+               for(int j=n-1;j>i+1;j--)
+               {
+                   a[j]=a[j-1];
+               }
+              
+               if(i+1<n) 
+               a[i+1]=0;
+                
                 i++;
             }
+            
         }
         
-        while(a.size()>n)
-        {
-            a.pop_back();
-        }
     }
 };
