7806341e29a9b49b1ccc291396cdc2ac0acb852f
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-08-13 23:50:23 +0530

    Time: 20 ms (13.34%), Space: 13.8 MB (26.08%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -4,22 +4,22 @@ public:
         
         int n=a.size();
         
-        int curr_mx=a[0],curr_mi=a[0];
-        int ans=a[0];
+        int mx=a[0],b=a[0],c=a[0];
         
         for(int i=1;i<n;i++)
         {
             if(a[i]<0)
             {
-                swap(curr_mx,curr_mi);
+                swap(b,c);
             }
             
-            curr_mx=max(a[i],curr_mx*a[i]);
-            curr_mi=min(a[i],curr_mi*a[i]);
+            b=max(a[i],b*a[i]);
             
-            ans=max(ans,curr_mx);
+            c=min(a[i],c*a[i]);
+            
+            mx=max(mx,b);
         }
         
-        return ans;
+        return mx;
     }
 };
