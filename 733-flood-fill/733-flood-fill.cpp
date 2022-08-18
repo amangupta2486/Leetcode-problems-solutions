e4a90e40d8d277267cd4ea793fac2025be21962c
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-08-18 23:03:29 +0530

    Time: 23 ms (16.72%), Space: 14 MB (46.49%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+    vector<vector<int>> floodFill(vector<vector<int>>& g, int sr, int sc, int color) {
         
-        int n=image.size();
-        int m=image[0].size();
+        int n=g.size();
+        int m=g[0].size();
         
-        int currcol=image[sr][sc];
-        
-        if(currcol==color)
+        if(g[sr][sc]==color)
         {
-            return image;
+            return g;
         }
         
-        int dx[4]={1,-1,0,0};
-        int dy[4]={0,0,1,-1};
+        int prev=g[sr][sc];
         
         queue<vector<int>> q;
         
-        image[sr][sc]=color;
+        g[sr][sc]=color;
         
         q.push({sr,sc});
+        
+        int dx[4]={1,-1,0,0};
+        int dy[4]={0,0,1,-1};
         
         while(!q.empty())
         {
@@ -38,16 +38,16 @@ public:
                     int x=i+dx[k];
                     int y=j+dy[k];
                     
-                    if(x>=0 && x<n && y>=0 && y<m && image[x][y]==currcol)
+                    if(x>=0 && x<n && y>=0 && y<m && g[x][y]==prev)
                     {
-                        image[x][y]=color;
+                        g[x][y]=color;
+                        
                         q.push({x,y});
                     }
                 }
-                
             }
         }
         
-        return image;
+        return g;
     }
 };
