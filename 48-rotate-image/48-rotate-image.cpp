95755f8f5bd2bdfc4b791f3d2162e90b34114363
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-08-18 14:32:04 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.72%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& a) {
+    void rotate(vector<vector<int>>& g) {
         
-        int n=a.size();
-        int m=a[0].size();
+        int n=g.size();
+        int m=g[0].size();
         
         for(int i=0;i<n;i++)
         {
-            for(int j=i;j<n;j++)
+            for(int j=i+1;j<m;j++)
             {
-                swap(a[i][j],a[j][i]);
+                swap(g[i][j],g[j][i]);
             }
         }
         
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n/2;j++)
+            for(int j=0;j<m/2;j++)
             {
-                swap(a[i][j],a[i][n-j-1]);
+                swap(g[i][j],g[i][n-j-1]);
             }
         }
         
