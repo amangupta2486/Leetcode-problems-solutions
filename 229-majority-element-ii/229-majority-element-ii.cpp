193c700126be004b4d8c7198fcb24a68b8533e1a
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-08-18 21:18:15 +0530

    Time: 22 ms (50.57%), Space: 15.9 MB (52.94%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& a){
+    vector<int> majorityElement(vector<int>& a) {
         
         int n=a.size();
-        int x1=-1,x2=-1;
+        
         int c1=0,c2=0;
+        
+        int x=0,y=0;
         
         for(int i=0;i<n;i++)
         {
-            if(x1==a[i])
+            
+            if(x==a[i])
             {
                 c1++;
             }
-            else if(x2==a[i])
+            
+            else if(y==a[i])
             {
                 c2++;
             }
-            else if(c1<=0)
+            
+            else if(c1==0)
             {
-                x1=a[i];
+                x=a[i];
                 c1++;
             }
-            else if(c2<=0)
+            
+            else if(c2==0)
             {
-                x2=a[i];
+                y=a[i];
                 c2++;
             }
+            
             else
             {
                 c1--;
@@ -33,29 +40,34 @@ public:
             }
         }
         
-        int r1=0,r2=0;
+        //cout<<x<<" "<<c1<<" "<<y<<" "<<c2<<endl;
+        
         vector<int> ans;
+        
+        int x1=0,x2=0;
         
         for(int i=0;i<n;i++)
         {
-            if(a[i]==x1)
+            if(x==a[i])
             {
-                r1++;
+                x1++;
             }
-            else if(a[i]==x2)
+            else if(y==a[i])
             {
-                r2++;
+                x2++;
             }
         }
         
-        if(r1>n/3)
+        
+        
+        if(x1>n/3)
         {
-            ans.push_back(x1);
+            ans.push_back(x);
         }
         
-        if(r2>n/3)
+        if(x2>n/3)
         {
-            ans.push_back(x2);
+            ans.push_back(y);
         }
         
         return ans;
