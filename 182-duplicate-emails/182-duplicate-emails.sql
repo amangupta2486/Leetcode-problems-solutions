2eb7f5a44d764e756c66e2afdf672114190f87da
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-08-20 11:28:56 +0530

    Time: 437 ms (52.18%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-select email from (select email,count(email) as num from Person group by email) as stats where num>1
+select email from Person group by email having count(email)>1
