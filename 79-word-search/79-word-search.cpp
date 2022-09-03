b37ce873ba335240484b7e6af4c297a6598ad77c
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-09-03 16:39:41 +0530

    Time: 447 ms (66.04%), Space: 8.3 MB (5.36%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     
+    int n,m;
+    
     int dx[4]={1,-1,0,0};
-    int dy[4]={0,0,1,-1};
+    int dy[4]={0,0,-1,1};
+    
     int ans=0;
     
-    void solve(int i,int j,vector<vector<char>>& b, string &w,int c,vector<vector<int>> &vis)
+    void solve(int i,int j,vector<vector<char>>& b, string &w,int l,vector<vector<int>> &v)
     {
-        if(c==w.size())
+        
+        if(l==w.size())
         {
-            //cout<<c<<" ";
             ans=1;
             return;
         }
@@ -19,20 +22,19 @@ public:
             int x=i+dx[k];
             int y=j+dy[k];
             
-            if(x>=0 && x<b.size() && y>=0 && y<b[0].size() && b[x][y]==w[c] && !vis[x][y])
+            if(x>=0 && x<n && y>=0 && y<m && b[x][y]==w[l] && v[x][y]==0)
             {
-                vis[x][y]=1;
-                solve(x,y,b,w,c+1,vis);
-                vis[x][y]=0;
+                v[x][y]=1;
+                solve(x,y,b,w,l+1,v);
+                v[x][y]=0;
             }
         }
-
     }
     
     bool exist(vector<vector<char>>& b, string w) {
-        ans=0;
-        int n=b.size();
-        int m=b[0].size();
+        
+        n=b.size();
+        m=b[0].size();
         
         for(int i=0;i<n;i++)
         {
@@ -40,18 +42,26 @@ public:
             {
                 if(b[i][j]==w[0])
                 {
-                    vector<vector<int>> vis(n,vector<int>(m,0));
-                    vis[i][j]=1;
-                    solve(i,j,b,w,1,vis);
+                    vector<vector<int>> v(n,vector<int>(m,0));
                     
-                    if(ans)
-                    {
-                        return 1;
-                    }
+                    v[i][j]=1;
+                    
+                    solve(i,j,b,w,1,v);
                 }
             }
         }
         
-        return 0;
+        return ans;
     }
 };
+
+/*
+
+
+[["A","B","C","E"],
+ ["S","F","E","S"],
+ ["A","D","E","E"]]
+ 
+ "ABCESEEEFS"
+
+*/
