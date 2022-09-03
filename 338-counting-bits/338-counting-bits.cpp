d324b6c0fac77e2343b1a3c09913dbb81f65ade6
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-09-03 10:22:43 +0530

    Time: 9 ms (52.91%), Space: 7.8 MB (72.60%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -4,12 +4,14 @@ public:
         
         vector<int> dp(n+1,0);
         
+        dp[0]=0;
+        
         if(n>0)
         dp[1]=1;
         
         for(int i=2;i<=n;i++)
         {
-            dp[i]= dp[i/2] + dp[i%2];    
+            dp[i]= dp[i/2] + dp[i%2];
         }
         
         return dp;
