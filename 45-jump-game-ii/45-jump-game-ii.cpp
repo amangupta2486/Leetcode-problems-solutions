26eab27d4925e8cb5a953ab772393b4f83c7fec2
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-09-05 00:59:43 +0530

    Time: 451 ms (34.21%), Space: 17.5 MB (19.71%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -15,16 +15,15 @@ public:
             return dp[i];
         }
         
-        int ans=10000;
+        int ans=100000;
         
-        for(int j=i;j<i+a[i] && j<n;j++)
+        for(int j=i;j<i+a[i] && j<n ;j++)
         {
-            ans=min(ans,1+solve(j+1,n,a));    
+            ans=min(ans,1+solve(j+1,n,a));
         }
         
         return dp[i]=ans;
     }
-    
     
     int jump(vector<int>& a) {
         
