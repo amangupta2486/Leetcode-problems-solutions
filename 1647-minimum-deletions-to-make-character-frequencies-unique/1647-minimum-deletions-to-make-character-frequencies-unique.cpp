1e6da3de5e00ad3235c6cf35f95f4a692f7e9955
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-09-13 23:53:07 +0530

    Time: 127 ms (37.87%), Space: 18.2 MB (7.63%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -6,50 +6,37 @@ public:
         
         vector<int> v(26,0);
         
-        int mx=0;
-        for(auto i:s)
+        for(int i=0;i<n;i++)
         {
-            v[i-'a']++;
-            mx=max(mx,i-'a');
+            v[s[i]-'a']++;
         }
+        
+        vector<vector<int>> p;
         
         for(int i=0;i<26;i++)
         {
-            mx=max(mx,v[i]);
+            p.push_back({v[i],i});
         }
+        
+        sort(p.rbegin(),p.rend());
+        
+        int mx=p[0][0];
         
         vector<int> vis(mx+1,0);
-        
-        
-        vector<vector<int>> b;
-        
-        for(int i=0;i<26;i++)
-        {
-            b.push_back({v[i],i});
-        }
-        
-        sort(b.rbegin(),b.rend());
         
         int ans=0;
         
         for(int i=0;i<26;i++)
         {
-            auto p=b[i];
-            int x=p[0];
-            int ch=p[1];
-            //cout<<x<<" "<<ch<<endl;
-            for(int j=x;j>0;j--)
+            int c=p[i][0];
+            
+            while(c>0 && vis[c]==1)
             {
-                if(vis[j])
-                {
-                    ans++;
-                }
-                else
-                {
-                    vis[j]=1;
-                    break;
-                }
+                ans++;
+                c--;
             }
+            
+            vis[c]=1;
         }
         
         return ans;
