49b554e1b60ba2fc121f4327076d89dc5dada0f7
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-09-16 18:34:15 +0530

    Time: 949 ms (19.77%), Space: 120 MB (68.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
     
-    //int dp[1005];
+    //int dp[100005];
     
-    long long solve(int i,int j,int n,int k,vector<int>& a, vector<int>& b,vector<vector<int>> &dp)
+    int solve(int i,int j,int k,int m,vector<int>& a, vector<int>& mul,vector<vector<int>> &dp)
     {
-        if(j<i || k==b.size())
+        if(i>j || k>=mul.size())
         {
             return 0;
         }
-        
-        if(i==j)
-        {
-            return dp[i][j]=a[i]*b[k];
-        }
-        
-        if(dp[k][i]!=-1)
+    
+        if(dp[k][i]!=-100000000)
         {
             return dp[k][i];
         }
         
-        long long l=a[i]*b[k] + solve(i+1,j,n,k+1,a,b,dp);
-        long long r=a[j]*b[k] + solve(i,j-1,n,k+1,a,b,dp);
+        int l=(a[i]*mul[k] )+ solve(i+1,j,k+1,m,a,mul,dp);
+        int r=(a[j]*mul[k] )+ solve(i,j-1,k+1,m,a,mul,dp);
         
         return dp[k][i]=max(l,r);
-        
     }
-    int maximumScore(vector<int>& a, vector<int>& b) {
+    
+    int maximumScore(vector<int>& a, vector<int>& mul) {
         
         int n=a.size();
-        int m=b.size();
+        int m=mul.size();
         
-        //memset(dp,-1,sizeof(dp));
+        vector<vector<int>> dp(m+1,vector<int>(m+1,-100000000));
         
-        vector<vector<int>> dp(m,vector<int>(m,-1));
-        
-        return solve(0,n-1,n,0,a,b,dp);
+        return solve(0,n-1,0,m,a,mul,dp);
     }
 };
