a28cd20961adc955c5275e8179a699ade53609b6
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-10-02 13:31:46 +0530

    Time: 87 ms (26.27%), Space: 20.1 MB (26.15%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& in) {
         
-        vector<vector<int>> merge;
+         vector<vector<int>> mr;
         
         sort(in.begin(),in.end());
         
-        for(auto x: in)
+        for(auto x:in)
         {
-            if(merge.empty() || merge.back()[1]<x[0])
+            if(mr.empty() || mr.back()[1]<x[0])
             {
-                merge.push_back(x);
+                mr.push_back(x);
             }
             else
             {
-                merge.back()[1]=max(merge.back()[1],x[1]);
+                mr.back()[1]=max(mr.back()[1],x[1]);
             }
         }
         
-        return merge;
+        return mr;
     }
 };
