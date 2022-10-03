2ebd2df9efa0f5d0dfb8e546fcc91868dc5c87a4
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-10-03 09:44:39 +0530

    Time: 359 ms (25.69%), Space: 95.4 MB (84.72%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -8,23 +8,27 @@ public:
         
         for(int i=0;i<n-1;i++)
         {
-            priority_queue<int,vector<int>,greater<int>> q;
+            //priority_queue<int,vector<int>,greater<int>> q;
+            
+            int s=0,f=0,mx=0;
             
             if(a[i]==a[i+1])
             {
-                q.push(t[i]);
+                s+=t[i];
+                mx=max(mx,t[i]);
+                f=1;
                 
                 while(i+1<n && a[i]==a[i+1])
                 {
                     i++;
-                    q.push(t[i]);
+                    s+=t[i];
+                    mx=max(mx,t[i]);
                 }
             }
             
-            while(q.size()>1)
+            if(f==1)
             {
-                ans+=q.top();
-                q.pop();
+                ans+=s-mx;
             }
         }
         
