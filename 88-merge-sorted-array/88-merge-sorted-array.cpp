9f27754216272b099ecfe7550d375d6a7dc130c1
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-10-03 18:20:07 +0530

    Time: 4 ms (58.57%), Space: 8.9 MB (99.13%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    void merge(vector<int>& a, int n, vector<int>& b, int m) {
+    void merge(vector<int>& a, int m, vector<int>& b, int n) {
         
-        int i=n-1,j=m-1;
+        int i=m-1,j=n-1;
         
         while(i>=0 && j>=0)
         {
@@ -14,16 +14,15 @@ public:
             
             else if(a[i]<b[j])
             {
-                a[i+j+1]= b[j];
+                a[i+j+1]=b[j];
                 j--;
             }
         }
         
         while(j>=0)
         {
-            a[i+j+1]=b[j];   
+            a[i+j+1]=b[j];
             j--;
         }
-        
     }
 };
