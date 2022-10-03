6416c2a401b54bd0ff3070272e52ea1913fc8505
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-10-03 09:41:47 +0530

    Time: 601 ms (5.08%), Space: 106.5 MB (5.94%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    int minCost(string s, vector<int>& t) {
+    int minCost(string a, vector<int>& t) {
         
-        int n=s.size();
+        int n=a.size();
         
         int ans=0;
         
         for(int i=0;i<n-1;i++)
         {
-            priority_queue<int,vector<int>,greater<int>> p;
+            priority_queue<int,vector<int>,greater<int>> q;
             
-            if(s[i]==s[i+1])
+            if(a[i]==a[i+1])
             {
-                while(i+1<n && s[i]==s[i+1])
-                {
-                    p.push(t[i]);
-                    i++;
-                }
+                q.push(t[i]);
                 
-                p.push(t[i]);
+                while(i+1<n && a[i]==a[i+1])
+                {
+                    i++;
+                    q.push(t[i]);
+                }
             }
             
-            while(p.size()>1)
+            while(q.size()>1)
             {
-                ans+=p.top();
-                p.pop();
+                ans+=q.top();
+                q.pop();
             }
         }
         
