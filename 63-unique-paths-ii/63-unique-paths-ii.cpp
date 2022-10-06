38e9a71be6c606e3a66b6e907c285328d4e59979
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-10-07 01:35:23 +0530

    Time: 8 ms (34.35%), Space: 7.6 MB (80.49%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     
+    int n,m;
+    
     int dp[105][105];
     
-    int solve(int i,int j,int n,int m,vector<vector<int>>& g)
+    int solve(int i,int j,vector<vector<int>>& g)
     {
         if(i>=n || j>=m || g[i][j]==1)
         {
@@ -20,15 +22,16 @@ public:
             return dp[i][j];
         }
         
-        return dp[i][j]=solve(i+1,j,n,m,g) + solve(i,j+1,n,m,g);
+        return dp[i][j]=solve(i+1,j,g)+solve(i,j+1,g);
     }
+    
     int uniquePathsWithObstacles(vector<vector<int>>& g) {
         
-        int n=g.size();
-        int m=g[0].size();
+        n=g.size();
+        m=g[0].size();
         
         memset(dp,-1,sizeof(dp));
         
-        return solve(0,0,n,m,g);
+        return solve(0,0,g);
     }
 };
