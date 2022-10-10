b7504e3b704b8115f555ae8fb3880ccd8c18d528
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-10-11 00:37:06 +0530

    Time: 4 ms (75.86%), Space: 13.9 MB (31.22%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    vector<string> ans;
+    
+    void solve(string s,int i,int j)
+    {
+        if(i==0 && j==0)
+        {
+            ans.push_back(s);
+            return;
+        }
+        
+        if(i>j)
+        {
+            return;
+        }
+        
+        if(i==0)
+        {
+            solve(s+')',i,j-1);
+            return;
+        }
+        
+        solve(s+'(',i-1,j);
+        solve(s+')',i,j-1);
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        
+        solve("",n,n);
+        
+        return ans;
+    }
+};
+
