3191c6881996223f0337ad4412fc3b508f8ee049
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-11-10 20:06:42 +0530

    Time: 6 ms (58.46%), Space: 7.6 MB (81.09%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    int dp[105][105];
+    
+    int solve(int i,int j,int n,int m,vector<vector<int>>& g)
+    {
+        if(i>=n || j>=m || g[i][j]==1)
+        {
+            return 0;
+        }
+        
+        if(i==n-1 && j==m-1)
+        {
+            return 1;
+        }
+    
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        
+        return dp[i][j]=solve(i+1,j,n,m,g) + solve(i,j+1,n,m,g);
+    }
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& g) {
+        
+        int n=g.size();
+        int m=g[0].size();
+        
+        memset(dp,-1,sizeof(dp));
+        
+        return solve(0,0,n,m,g);
+    }
+};
