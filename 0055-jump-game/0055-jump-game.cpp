b71d9d4abf3b351f8539092febad072300eb981b
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-11-10 20:18:22 +0530

    Time: 160 ms (55.95%), Space: 48.5 MB (45.91%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canJump(vector<int>& a) {
+        
+        int n=a.size();
+        
+        int l=n-1;
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            if(i+a[i]>=l)
+            {
+                l=i;
+            }
+        }
+        
+        return l==0;
+    }
+};
