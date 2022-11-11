a869f921a7231918183f035db03b6bc9f8f34263
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-11-11 18:46:15 +0530

    Time: 8 ms (51.57%), Space: 9.7 MB (36.55%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    unordered_set<int> s;
+    
+    int dp[400];
+    
+    int solve(int i,vector<int>& d, vector<int>& c)
+    {
+        if(i>365)
+        {
+            return 0;
+        }
+        
+        if(dp[i]!=-1)
+        {
+            return dp[i];
+        }
+        
+        int ans=0;
+        
+        if(s.find(i)!=s.end())
+        ans=min({c[0]+solve(i+1,d,c),c[1] + solve(i+7,d,c),c[2] + solve(i+30,d,c)});
+        
+        else
+        ans=solve(i+1,d,c);
+        
+        return dp[i]=ans;
+    }
+    int mincostTickets(vector<int>& d, vector<int>& c) {
+        
+        for(auto x:d)
+        {
+            s.insert(x);
+        }
+        
+        memset(dp,-1,sizeof(dp));
+        
+        return solve(1,d,c);    
+    }
+};
