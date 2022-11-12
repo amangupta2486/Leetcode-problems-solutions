90f8eb22c114209e534e1409b6297be9b9890aff
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-11-12 23:27:10 +0530

    Time: 8 ms (35.13%), Space: 7.9 MB (39.43%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    vector<vector<int>> ans;
+    
+    void solve(int i,int n,vector<int>& a)
+    {
+        if(i==n)
+        {
+            ans.push_back(a);
+            return;
+        }
+        
+        for(int j=i;j<n;j++)
+        {
+            swap(a[i],a[j]);
+            solve(i+1,n,a);
+            swap(a[i],a[j]);
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& a) {
+        
+        int n=a.size();
+        
+        solve(0,n,a);
+        
+        return ans;
+    }
+};
