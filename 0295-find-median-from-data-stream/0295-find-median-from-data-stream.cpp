5b0c2463102d5a3f16617bb2f9dc202940557ebd
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-11-12 11:56:10 +0530

    Time: 928 ms (6.15%), Space: 119 MB (10.29%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -0,0 +1,95 @@
+class MedianFinder {
+public:
+    
+    priority_queue<double> pq;
+     priority_queue<double,vector<double>,greater<double>> pq1;
+    
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        
+        if(pq.empty())
+        {
+            pq.push(num);
+        }
+        
+        else
+        {
+            if(num>pq.top())
+            {
+                pq1.push(num);
+                
+                int n=pq.size();
+                int m=pq1.size();
+                
+                while(pq1.size()>pq.size())
+                {
+                    auto t=pq1.top();
+                    pq1.pop();
+
+                    pq.push(t);
+                }
+                
+//                 if(pq.size()>pq1.size())
+//                 {
+//                     pq1.push(num);
+//                 }
+
+//                 else
+//                 {
+//                     auto t=pq1.top();
+//                     pq1.pop();
+
+//                     pq.push(t);
+//                     pq1.push(num);
+//                 }
+            }
+            else
+            {
+                pq.push(num);
+                
+                int n=pq.size();
+                int m=pq1.size();
+                
+                 while(pq1.size()<pq.size())
+                {
+                    auto t=pq.top();
+                    pq.pop();
+
+                    pq1.push(t);
+                }
+                
+                if((n+m)%2!=0)
+                {
+                    auto t=pq1.top();
+                    pq1.pop();
+
+                    pq.push(t);
+                }
+            }
+            
+        }
+        
+    }
+    
+    double findMedian() {
+        
+        if(pq.size()>pq1.size())
+        {
+            return pq.top();
+        }
+        
+        double ans=(pq.top()+pq1.top())/2.0;
+        
+        return ans;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
