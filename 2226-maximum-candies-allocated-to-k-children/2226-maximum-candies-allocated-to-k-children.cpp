373b81c1f611a9e41887b9fa1229283da2315b3f
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-11-17 13:14:42 +0530

    Time: 256 ms (88.74%), Space: 84.4 MB (17.92%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -1,55 +1,52 @@
 class Solution {
 public:
-    int maximumCandies(vector<int>& candies, long long k) {
+    int maximumCandies(vector<int>& c, long long k) {
         
-        int n=candies.size();
-        
-        int l=1,r=0;
         long long s=0;
+        
+        int n=c.size();
         
         for(int i=0;i<n;i++)
         {
-            r=max(r,candies[i]);
-            s+=candies[i];
+            s+=c[i];
         }
         
-        if(k>s)
+        if(s<k)
         {
             return 0;
         }
+        
+        int l=1,r=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            r=max(r,c[i]);
+        }
+        
         int ans=0;
         
         while(l<=r)
         {
-            int m=(l+r)/2;
+            int m=l+(r-l)/2;
             
-            long long c=0;
+            long long cnt=0;
             
             for(int i=0;i<n;i++)
             {
-                c+=candies[i]/m;   
+                cnt=cnt+(c[i]/m);
             }
             
-            if(c<k)
-            {
-                r=m-1;
-            }
-            else
+            if(cnt>=k)
             {
                 ans=m;
                 l=m+1;
+            }
+            else
+            {
+                r=m-1;
             }
         }
         
         return ans;
     }
 };
-/*
-Input
-[5,6,4,10,10,1,1,2,2,2]
-9
-Output
-0
-Expected
-3
-*/
