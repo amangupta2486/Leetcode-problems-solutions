0ce457264fedf6d0adf8f81efe02379e6e6d25e6
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-11-21 13:37:16 +0530

    Time: 1516 ms (16.67%), Space: 202.4 MB (16.67%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,64 +1,50 @@
-
 class Solution {
 public:
+    
+    long long ans=0;
+    
+    int dfs(int i,int prev,vector<int> v[], int s,vector<int> &vis)
+    {
+        vis[i]=1;
+        
+        long long cnt=1;
+        
+        for(auto j:v[i])
+        {
+            if(!vis[j])
+            {
+                cnt+=dfs(j,i,v,s,vis);
+            }
+        }
+        
+        if(i!=0)
+        {
+            ans+=cnt/s;
+        
+            if(cnt%s)
+            {
+                ans++;
+            }
+        }
+        
+        
+        return cnt;
+    }
+    
     long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
         
         int n=roads.size();
         
-        vector<int> people(n+1,1),degree(n+1,0);
-        people[0]=0;
-        
         vector<int> v[n+1];
+        vector<int> vis(n+1,0);
         
-        for(auto x:roads){
+        for(auto x:roads)
+        {
             v[x[0]].push_back(x[1]);
             v[x[1]].push_back(x[0]);
-            
-            degree[x[0]]++;
-            degree[x[1]]++;
         }
         
-        queue<int> q;
-        
-        for(int i=1;i<n+1;i++){
-            if(degree[i]==1)   {
-                q.push(i);
-            }
-        }
-        
-        long long ans=0;
-        
-        while(!q.empty())
-        {
-            int l=q.size();
-            
-            while(l--){
-                auto city=q.front();
-                q.pop();
-                
-                if(city==0){
-                    break;
-                }
-                
-                ans+=people[city]/seats;
-
-                if(people[city]%seats){
-                    ans++;
-                }
-                
-                degree[city]=0;
-                
-                for(auto x:v[city]){
-                    people[x]+=people[city];
-                    degree[x]--;
-                    
-                    if(degree[x]==1)
-                    {
-                        q.push(x);
-                    }
-                }
-            }
-        }
+        dfs(0,0,v,seats,vis);
         
         return ans;
     }
