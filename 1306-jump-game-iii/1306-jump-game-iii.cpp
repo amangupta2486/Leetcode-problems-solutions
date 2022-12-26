ea348165f0bd50af62093f63bdb19301e6526db1
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2022-12-26 11:43:49 +0530

    Time: 56 ms (79.56%), Space: 35 MB (40.19%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    
     int ans=0;
-    
     int vis[50005];
-    
     void solve(int i,int n,vector<int>& a)
     {
-        if(a[i]==0)
+        if(i<0 || i>=n)
         {
-            ans=1;
+            return;
         }
         
         if(vis[i])
@@ -17,23 +14,23 @@ public:
             return;
         }
         
+        if(a[i]==0)
+        {
+            ans=1;
+        }
+        
         vis[i]=1;
         
-        if(i-a[i]>=0)
-        solve(i-a[i],n,a);
-        
-        if(i+a[i]<n)
         solve(i+a[i],n,a);
-        
+        solve(i-a[i],n,a);
     }
     
     bool canReach(vector<int>& a, int start) {
         
         int n=a.size();
+        solve(start,n,a);
         
         memset(vis,0,sizeof(vis));
-        
-        solve(start,n,a);
         
         return ans;
     }
