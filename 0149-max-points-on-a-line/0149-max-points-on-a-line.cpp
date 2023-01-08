9962dc2799ce6b4b346daf55ca287f035f82437a
Aman Gupta <53896877+amangupta2486@users.noreply.github.com>
2023-01-08 13:03:44 +0530

    Time: 37 ms (87.56%), Space: 12.5 MB (66.56%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& p) {
+        
+        int n=p.size(),ans=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            int same=1;
+            map<double,int> mp;
+            
+            for(int j=i+1;j<n;j++)
+            {
+                if(p[j][1]==p[i][1] && p[j][0]==p[i][0])
+                {
+                    same++;
+                }
+                
+                else if(p[j][0]==p[i][0])
+                {
+                    mp[INT_MAX]++;
+                }
+                
+                else
+                {
+                    double y=p[j][1]-p[i][1];
+                    double x=p[j][0]-p[i][0];
+                    
+                    double d=y/x;
+                    
+                    mp[d]++;
+                }
+                
+            }
+            
+            int lmx=0;
+            
+            for(auto x:mp)
+            {
+                lmx=max(lmx,x.second);
+            }
+            
+            lmx+=same;
+            
+            ans=max(ans,lmx);
+        }
+        
+        return ans;
+    }
+};
